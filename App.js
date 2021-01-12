403d55e07a07bbdbac4a449e3b021baea7f24ccf
Gilbert Hsu <45667486+gilhsu@users.noreply.github.com>
2021-01-12 14:49:56 -0500

    Setup Expo App, Clone Auth Demo (#1)
    * expo install expo-app-auth
    
    * yarn add fhir-kit-client
    
    * implement demo login
    
    cleanup nav samples
    
    * autofix
    
    * yarn add prop-types
    
    * fix linting
    
    * fix proptypes
    
    * rename demologin

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  Image,
+} from 'react-native';
+import Login from './src/components/Login/Login';
+import logo from './assets/images/vermonster-logo.png';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar
-        style="auto" // eslint-disable-line react/style-prop-object
-      />
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={styles.scrollView}
+      >
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.vlogo}
+            source={logo}
+            resizeMode="contain"
+          />
+          <Image
+            style={styles.slogo}
+            source={{ uri: 'http://syncfor.science/s4s-logo.png' }}
+            resizeMode="contain"
+          />
+        </View>
+        <View style={styles.description}>
+          <Text>Discovery Mobile App</Text>
+        </View>
+        <Login />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  safeAreaView: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  scrollView: {
+    height: '100%',
+    padding: 20,
+  },
+
+  vlogo: {
+    height: 50,
+    width: '60%',
+  },
+  slogo: {
+    height: 50,
+    width: '60%',
+  },
+  logoContainer: {
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 25,
+    marginBottom: 25,
+  },
+
+  description: {
+    alignItems: 'center',
   },
 });
---
+++ src/components/Login/Login.js
@@ -0,0 +1,186 @@
+import React, { useState, useEffect } from 'react';
+import { func, shape } from 'prop-types';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import * as AppAuth from 'expo-app-auth';
+import Client from 'fhir-kit-client';
+
+// smartapp auth with provided patient
+const fhirIss = 'https://launch.smarthealthit.org/v/r4/sim/eyJrIjoiMSIsImIiOiI2ODk4OTJiZC1kY2JlLTQxZmMtODY1MS0zOGExZDA4OTM4NTQifQ/fhir';
+
+const initializeFhirClient = (baseUrl, accessToken) => {
+  if (!accessToken) {
+    return new Client({ baseUrl });
+  }
+  return new Client({
+    baseUrl,
+    customHeaders: {
+      Authorization: `Bearer ${accessToken}`,
+    },
+  });
+};
+
+export async function signInAsync() {
+  const fhirClient = initializeFhirClient(fhirIss);
+  const { authorizeUrl, tokenUrl } = await fhirClient.smartAuthMetadata();
+
+  const config = {
+    serviceConfiguration: {
+      authorizationEndpoint: authorizeUrl.toString(),
+      tokenEndpoint: tokenUrl.toString(),
+    },
+    additionalParameters: {
+      aud: fhirIss,
+    },
+    clientId: 'example-client-id',
+    clientSecret: 'example-client-secret',
+    // redirectUrl:
+    //   Platform.OS === "android"
+    //     ? "com.reactnativeoauth:/callback"
+    //     : "org.reactjs.native.example.ReactNativeOauth:/callback",
+    scopes: [
+      'openid',
+      'fhirUser',
+      'patient/*.*',
+      'launch/patient',
+      'online_access',
+    ],
+  };
+
+  let result;
+  try {
+    result = await AppAuth.authAsync(config);
+    console.log('Auth result from signInAsync', result);
+  } catch (error) {
+    console.warn('error', error);
+  }
+
+  return result;
+}
+
+const Login = () => {
+  const [authResult, setAuthResult] = useState(null);
+  const [patient, setPatient] = useState(null);
+
+  useEffect(() => {
+    if (authResult && !patient) {
+      const {
+        accessToken,
+        additionalParameters: { patient: patientId },
+      } = authResult;
+      const fhirClient = initializeFhirClient(fhirIss, accessToken);
+      const queryPatient = async () => {
+        try {
+          const patientData = await fhirClient.read({
+            resourceType: 'Patient',
+            id: patientId,
+          });
+          setPatient(patientData);
+        } catch (error) {
+          setPatient(error);
+        }
+      };
+      queryPatient();
+    }
+  }, [authResult, patient]);
+
+  return (
+    <View>
+      {patient ? (
+        <PatientView authResult={authResult} patient={patient} />
+      ) : (
+        <View style={styles.body}>
+          <LoginButton
+            handleAuthorize={async () => {
+              const authResponse = await signInAsync();
+              setAuthResult(authResponse);
+            }}
+          />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default Login;
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    justifyContent: 'space-between',
+  },
+  section: {
+    overflow: 'scroll',
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 10,
+  },
+  body: {
+    alignItems: 'center',
+  },
+  login: {
+    backgroundColor: '#db882a',
+    height: 50,
+    borderRadius: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '50%',
+    marginTop: 100,
+  },
+  loginText: {
+    color: 'white',
+    fontSize: 20,
+  },
+  scrollViewInternal: {
+    height: 240,
+    padding: 20,
+    borderWidth: 1,
+    borderColor: 'lightgray',
+  },
+});
+
+const LoginButton = ({ handleAuthorize }) => (
+  <TouchableOpacity style={styles.login} onPress={handleAuthorize}>
+    <Text style={styles.loginText}>Login</Text>
+  </TouchableOpacity>
+);
+
+LoginButton.propTypes = {
+  handleAuthorize: func.isRequired,
+};
+
+const PatientView = ({ authResult, patient }) => (
+  <View style={styles.sectionContainer}>
+    <View style={styles.section}>
+      <Text style={styles.title}>Authorization Result:</Text>
+      <ScrollView
+        style={styles.scrollViewInternal}
+        nestedScrollEnabled
+      >
+        <Text style={styles.text}>{JSON.stringify(authResult, null, 2)}</Text>
+      </ScrollView>
+    </View>
+    <View style={styles.section}>
+      <Text style={styles.title}>Patient:</Text>
+      <ScrollView
+        style={styles.scrollViewInternal}
+        nestedScrollEnabled
+      >
+        <Text>{JSON.stringify(patient, null, 2)}</Text>
+      </ScrollView>
+    </View>
+  </View>
+);
+
+PatientView.propTypes = {
+  authResult: shape({}).isRequired,
+  patient: shape({}).isRequired,
+};
