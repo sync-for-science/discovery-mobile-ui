4dd5329d01df900741274c8f431aa282614e52da
Gilbert Hsu <gilbert@vermonster.com>
2021-02-22 09:48:25 -0500

    create timeline widget and category selector placeholders

--- src/screens/CatalogScreen.js
+++ src/screens/CatalogScreen.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {
   StyleSheet, Text, View, SafeAreaView, StatusBar,
 } from 'react-native';
+
+import TimelineWidget from '../components/Timeline/TimelineWidget'
+import CategorySelector from '../components/CategorySelector/CategorySelector'
 import Colors from '../constants/Colors';
 
 const CatalogScreen = () => (
   <SafeAreaView style={styles.safeAreaView}>
     <StatusBar backgroundColor={Colors.primary} barStyle="dark-content" />
-    <View style={styles.screen}>
-      <Text>Catalog Screen</Text>
+    <View style={styles.content}>
+      <TimelineWidget />
+      <CategorySelector />
     </View>
   </SafeAreaView>
 );
@@ -18,10 +22,14 @@ export default CatalogScreen;
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
+    alignItems: 'center'
   },
   screen: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
+  content: {
+    width: '90%',
+  }
 });
