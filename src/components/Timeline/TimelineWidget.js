97384287fbd417e90e0c2730ca5a395e5b5ee6a3
Gilbert Hsu <gilbert@vermonster.com>
2021-02-22 14:38:50 -0500

    linting

--- src/components/Timeline/TimelineWidget.js
+++ src/components/Timeline/TimelineWidget.js
@@ -2,17 +2,13 @@ import React from 'react';
 import {
   StyleSheet, Text, View,
 } from 'react-native';
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 
-const CatalogScreen = () => {
-
-  return(
-    <View style={styles.root}>
-      <Text>Timeline Widget</Text>
-    </View>
-  )
-}
-
+const CatalogScreen = () => (
+  <View style={styles.root}>
+    <Text>Timeline Widget</Text>
+  </View>
+);
 
 export default CatalogScreen;
 
