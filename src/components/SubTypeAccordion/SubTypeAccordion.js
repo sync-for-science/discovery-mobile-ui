42cc65e63fdb324852044abba97e315205e71922
Gilbert Hsu <gilbert@vermonster.com>
2021-02-25 18:49:09 -0500

    display data in SubTypeAccordion

--- src/components/SubTypeAccordion/SubTypeAccordion.js
+++ src/components/SubTypeAccordion/SubTypeAccordion.js
@@ -1,12 +1,45 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import { Accordion, Icon} from "native-base";
+
+import Colors from '../../constants/Colors'
 
 const SubTypeAccordion = ({subType, resourcesIds}) => {
-  console.log('subType', subType)
-  console.log('resourcesIds', resourcesIds)
+  const dataArray = [
+    {title: subType, content: resourcesIds},
+  ]
+
+  const renderHeader = (item, expanded) => {
+    return (
+      <View style={{
+        flexDirection: "row",
+        padding: 10,
+        justifyContent: "space-between",
+        alignItems: "center" ,
+        backgroundColor: Colors.primary 
+      }} >
+        <Text style={{color: 'white'}}>{item.title}</Text>
+        {expanded
+          ? <Icon style={{ fontSize: 18 }} name="remove-circle" />
+          : <Icon style={{ fontSize: 18 }} name="add-circle" />}
+      </View>
+    )
+  }
+
+  const renderContent = (item) => {
+    return item.content.map(resourceId => <View><Text>{resourceId}</Text></View>)
+  }
+
   return (
-    <View>
-      <Text>{subType}</Text>
+    <View style={{marginBottom: 10}}>
+      <Accordion 
+        dataArray={dataArray}
+        icon="add" 
+        iconStyle={{ color: "green" }}
+        expanded={[0]}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+      />
     </View>
   )
 }
