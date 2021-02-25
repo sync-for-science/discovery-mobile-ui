1108c2c9f72cff1ae60a30a28dc1b1467d563c13
Gilbert Hsu <45667486+gilhsu@users.noreply.github.com>
2021-02-25 15:13:19 -0500

    Add Placeholder FilterDrawer (#16)
    * hook up filterDrawer
    
    * linting
    
    * disable swipe back navigation
    
    * linting

--- src/screens/CatalogScreen.js
+++ src/screens/CatalogScreen.js
@@ -7,15 +7,18 @@ import TimelineWidget from '../components/Timeline/TimelineWidget';
 import CategorySelector from '../components/CategorySelector/CategorySelector';
 import RecordCardsContainer from '../components/RecordCard/RecordCardsContainer';
 import Colors from '../constants/Colors';
+import FilterDrawer from '../components/FilterDrawer/FilterDrawer';
 
 const CatalogScreen = () => (
   <SafeAreaView style={styles.safeAreaView}>
     <StatusBar backgroundColor={Colors.primary} barStyle="dark-content" />
-    <ScrollView style={styles.content}>
-      <TimelineWidget />
-      <CategorySelector />
-      <RecordCardsContainer />
-    </ScrollView>
+    <FilterDrawer>
+      <ScrollView style={styles.content}>
+        <TimelineWidget />
+        <CategorySelector />
+        <RecordCardsContainer />
+      </ScrollView>
+    </FilterDrawer>
   </SafeAreaView>
 );
 
@@ -24,7 +27,6 @@ export default CatalogScreen;
 const styles = StyleSheet.create({
   safeAreaView: {
     flex: 1,
-    alignItems: 'center',
   },
   screen: {
     flex: 1,
