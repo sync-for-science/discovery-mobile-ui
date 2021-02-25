14b5a581ce14edf18d29d0ab850efc6bb3c762d7
Gilbert Hsu <gilbert@vermonster.com>
2021-02-25 18:17:58 -0500

    cleanup RecordCardsContainer

--- src/components/RecordCard/RecordCardsContainer.js
+++ src/components/RecordCard/RecordCardsContainer.js
@@ -3,9 +3,8 @@ import {
   StyleSheet, Text, View,
 } from 'react-native';
 import {connect} from 'react-redux'
-import SubTypeAccordion from '../SubTypeAccordion/SubTypeAccordion';
 
-import RecordCard from './RecordCard';
+import SubTypeAccordion from '../SubTypeAccordion/SubTypeAccordion';
 
 const RecordCardsContainer = ({resourceIdsGroupedByType}) => {
   const selectedCategory = "Condition"
@@ -13,7 +12,6 @@ const RecordCardsContainer = ({resourceIdsGroupedByType}) => {
   
   return(
     <View style={styles.root}>
-      <Text>RecordCards Container</Text>
       <View style={styles.container}>
         {Object.keys(categorySubTypes).map(subType => {
           const resourcesIds = Array.from(categorySubTypes[subType])
@@ -34,10 +32,6 @@ export default connect(mapStateToProps, null)(RecordCardsContainer);
 const styles = StyleSheet.create({
   root: {
     width: '100%',
-    padding: 10,
-    backgroundColor: 'lightgray',
-    borderWidth: 1,
-    borderColor: 'gray',
     marginVertical: 20,
     justifyContent: 'center',
     alignItems: 'center',
