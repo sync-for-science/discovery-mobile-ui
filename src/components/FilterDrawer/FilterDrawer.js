0811b38bb92d7b6fe1727c14462fab1e68b947c4
Gilbert Hsu <gilbert@vermonster.com>
2021-02-26 12:32:29 -0500

    hookup Categories to FilterDrawer

--- src/components/FilterDrawer/FilterDrawer.js
+++ src/components/FilterDrawer/FilterDrawer.js
@@ -8,34 +8,40 @@ import {
 } from 'react-native';
 import { DrawerLayout } from 'react-native-gesture-handler';
 import { widthPercentageToDP as wp } from 'react-native-responsive-screen';
+import {connect} from 'react-redux'
 
-const FilterDrawer = ({ children }) => {
+import RESOURCE_TYPES from '../../resources/resourceTypes';
+
+const FilterDrawer = ({ resourceIdsGroupedByType, children }) => {
+
+  const CategoryFilter = ({subType}) => {
+    const label = RESOURCE_TYPES[subType]
+    return (
+      <View style={styles.categoryRow}>
+        <Text>{label}</Text>
+        <Switch />
+      </View>
+    )
+  }
+
   const renderDrawer = () => (
     <View style={styles.drawerContainer}>
       <Text style={styles.drawerTitle}>Category Filters</Text>
-      <View style={styles.categoryRow}>
-        <Text>Category</Text>
-        <Switch />
-      </View>
-      <View style={styles.categoryRow}>
-        <Text>Category</Text>
-        <Switch />
-      </View>
-      <View style={styles.categoryRow}>
-        <Text>Category</Text>
-        <Switch />
-      </View>
-      <View style={styles.categoryRow}>
-        <Text>Category</Text>
-        <Switch />
-      </View>
+      {Object.keys(resourceIdsGroupedByType).map(subType => {
+        if (resourceIdsGroupedByType[subType].filterable) {
+          return (
+            <CategoryFilter key={subType} subType={subType} />
+          )
+        }
+        return null
+      })}
     </View>
   );
 
   return (
     <View style={styles.container}>
       <DrawerLayout
-        drawerWidth={wp('50%')}
+        drawerWidth={wp('60%')}
         keyboardDismissMode="on-drag"
         drawerPosition={DrawerLayout.positions.Left}
         drawerType="front"
@@ -54,7 +60,11 @@ FilterDrawer.propTypes = {
   children: node.isRequired,
 };
 
-export default FilterDrawer;
+const mapStateToProps = (state) => ({
+  resourceIdsGroupedByType: state.resourceIdsGroupedByType
+})
+
+export default connect(mapStateToProps, null)(FilterDrawer);
 
 const styles = StyleSheet.create({
   container: {
