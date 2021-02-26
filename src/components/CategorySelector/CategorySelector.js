8c7d0cf7a29bc20f67e9153651810c8b02beb0e3
Gilbert Hsu <gilbert@vermonster.com>
2021-02-26 09:04:28 -0500

    hook up CategorySelector with RecordCardContainer

--- src/components/CategorySelector/CategorySelector.js
+++ src/components/CategorySelector/CategorySelector.js
@@ -3,46 +3,61 @@ import {
   StyleSheet, Text, View, ScrollView,
 } from 'react-native';
 
-const CatalogScreen = () => (
-  <ScrollView style={styles.root} horizontal>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-    <View style={styles.button}>
-      <Text style={styles.buttonText}>Category</Text>
-    </View>
-  </ScrollView>
-);
+import { Button } from 'native-base'
+
+import Colors from '../../constants/Colors'
+import RESOURCE_TYPES from '../../resources/resourceTypes'
+
+const CatalogScreen = ({categories, selectedCategory, setSelectedCategory}) => {
+  const CategoryButton = ({category}) => {
+    const categoryDisplay = RESOURCE_TYPES[category]
+    const buttonStyle = category === selectedCategory ? styles.buttonSelected : styles.button
+    const buttonTextStyle = category === selectedCategory ? styles.buttonSelectedText : null
+    return (
+      <Button style={buttonStyle} onPress={() => setSelectedCategory(category)}>
+        <Text style={buttonTextStyle}>{categoryDisplay}</Text>
+      </Button>
+    )
+  }
+
+  const scrubbedCategories = categories.filter(category => category !== "Patient" && category !== "Observation")
+
+  return(
+    <ScrollView style={styles.root} horizontal>
+      {scrubbedCategories.map(category => {
+        if (category !== "Patient") {
+          return <CategoryButton key={category} category={category} />
+        }
+      })}
+    </ScrollView>
+  )
+}
+
 
 export default CatalogScreen;
 
 const styles = StyleSheet.create({
   root: {
-    backgroundColor: 'lightgray',
-    borderWidth: 1,
+    backgroundColor: Colors.primaryLight,
     borderColor: 'gray',
     marginTop: 20,
     flexDirection: 'row',
     padding: 10,
   },
   button: {
-    padding: 10,
+    paddingHorizontal: 10,
+    marginHorizontal: 10,
+    backgroundColor: 'white',
+    borderRadius: 30,
+  },
+  buttonSelected: {
+    paddingHorizontal: 10,
     marginHorizontal: 10,
     borderWidth: 2,
-    borderColor: 'black',
-    backgroundColor: 'white',
-    borderRadius: 20,
+    backgroundColor: Colors.primary,
+    borderRadius: 30,
   },
+  buttonSelectedText: {
+    color: 'white'
+  }
 });
