9d300a28c55d6e3ecce27d21e4212005ab37c0cc
Gilbert Hsu <gilbert@vermonster.com>
2021-02-26 09:35:21 -0500

    lint-fix and preselect category

--- src/components/SubTypeAccordion/SubTypeAccordion.js
+++ src/components/SubTypeAccordion/SubTypeAccordion.js
@@ -1,53 +1,55 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Accordion } from "native-base";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Accordion } from 'native-base';
 import { Ionicons } from '@expo/vector-icons';
 
-import Colors from '../../constants/Colors'
+import Colors from '../../constants/Colors';
 
-const SubTypeAccordion = ({subType, resourcesIds}) => {
+const SubTypeAccordion = ({ subType, resourcesIds }) => {
   const dataArray = [
-    {title: subType, content: resourcesIds},
-  ]
+    { title: subType, content: resourcesIds },
+  ];
 
-  const renderHeader = (item, expanded) => {
-    return (
-      <View style={{
-        flexDirection: "row",
-        padding: 10,
-        justifyContent: "space-between",
-        alignItems: "center" ,
-        backgroundColor: Colors.primary,
-      }} >
-        <View style={{width: '80%'}}>
-          <Text style={{color: 'white'}}>{item.title} [{item.content.length}]</Text>
-        </View>
-        { expanded
-          ? <Ionicons name="caret-down" size={20} color='white' />
-          : <Ionicons name="caret-up" size={20} color='white' />
-        }
+  const renderHeader = (item, expanded) => (
+    <View style={{
+      flexDirection: 'row',
+      padding: 10,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      backgroundColor: Colors.primary,
+    }}
+    >
+      <View style={{ width: '80%' }}>
+        <Text style={{ color: 'white' }}>
+          {item.title}
+          {' '}
+          [
+          {item.content.length}
+          ]
+        </Text>
       </View>
-    )
-  }
+      { expanded
+        ? <Ionicons name="caret-down" size={20} color="white" />
+        : <Ionicons name="caret-up" size={20} color="white" />}
+    </View>
+  );
 
-  const renderContent = (item) => {
-    return item.content.map(resourceId => <View style={{backgroundColor: 'white', padding: 10}}><Text>{resourceId}</Text></View>)
-  }
+  const renderContent = (item) => item.content.map((resourceId) => <View style={{ backgroundColor: 'white', padding: 10 }}><Text>{resourceId}</Text></View>);
 
   return (
-    <View style={{marginBottom: 10}}>
-      <Accordion 
+    <View style={{ marginBottom: 10 }}>
+      <Accordion
         dataArray={dataArray}
-        icon="add" 
-        iconStyle={{ color: "green" }}
+        icon="add"
+        iconStyle={{ color: 'green' }}
         expanded={[]}
         renderHeader={renderHeader}
         renderContent={renderContent}
       />
     </View>
-  )
-}
+  );
+};
 
-export default SubTypeAccordion
+export default SubTypeAccordion;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
