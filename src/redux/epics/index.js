26e50a70897f245d73da1501f1cff0b16958ecb7
erwin foxtree <foxtree@users.noreply.github.com>
2021-02-27 00:47:42 -0500

    src/redux/epics/index.js: extract and follow pagination links

--- src/redux/epics/index.js
+++ src/redux/epics/index.js
@@ -1,7 +1,12 @@
+import { from, of } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 import {
   createEpicMiddleware, combineEpics, ofType,
 } from 'redux-observable';
-import { map } from 'rxjs/operators';
+import {
+  catchError, map, concatMap,
+} from 'rxjs/operators';
+import { pathEq } from 'ramda';
 
 export const actionTypes = {
   // SET_PATIENT_DATA string is implicitly derived from:
@@ -9,6 +14,7 @@ export const actionTypes = {
   SET_PATIENT_DATA: 'patientData/setPatientData',
   CLEAR_PATIENT_DATA: 'patientData/clearPatientData',
   FLATTEN_RESOURCES: 'FLATTEN_RESOURCES',
+  REQUEST_NEXT_ITEMS: 'REQUEST_NEXT_ITEMS',
   GROUP_BY_TYPE: 'GROUP_BY_TYPE',
 };
 
@@ -31,11 +37,63 @@ const groupByType = (action$, state$) => action$.pipe(
   }),
 );
 
+const extractLinksFromBundle = (resourceBundle) => {
+  if (resourceBundle.resourceType === 'Bundle') {
+    const accumulatedLinks = [];
+    const hasNextLink = pathEq(['relation'], 'next');
+
+    const topLinkObject = resourceBundle?.link?.find(hasNextLink);
+    if (topLinkObject?.url) {
+      accumulatedLinks.push(topLinkObject.url);
+    }
+
+    return resourceBundle.entry.reduce((acc, item) => {
+      const nextLinkObject = item?.resource?.link?.find(hasNextLink);
+      if (nextLinkObject) {
+        acc.push(nextLinkObject.url);
+      }
+      return acc;
+    }, accumulatedLinks);
+  }
+  return [];
+};
+
+const handleError = (error, message) => {
+  console.error(`${message}: `, error); // eslint-disable-line no-console
+  return of({
+    type: 'ERROR',
+    error: true,
+    payload: { message, error },
+  });
+};
+
+const requestNextItems = (action$, state$, { rxAjax }) => action$.pipe(
+  ofType(actionTypes.FLATTEN_RESOURCES),
+  // delay(1000),
+  concatMap(({ payload }) => {
+    const accumulatedLinks = extractLinksFromBundle(payload);
+    // console.info('accumulatedLinks: ', JSON.stringify(accumulatedLinks));
+    const nextRequests$ = from(accumulatedLinks).pipe(
+      concatMap((url) => rxAjax.getJSON(url)),
+    );
+    return nextRequests$.pipe(
+      map((result) => ({
+        // type: actionTypes.FLATTEN_RESOURCES,
+        type: actionTypes.SET_PATIENT_DATA,
+        payload: result,
+      })),
+      catchError((error) => handleError(error, 'Error in requestNextItems nextRequests$.pipe')),
+    );
+  }),
+  catchError((error) => handleError(error, 'Error in requestNextItems concatMap')),
+);
+
 export const rootEpic = combineEpics(
   flattenResources,
   groupByType,
+  requestNextItems,
 );
 
 export default createEpicMiddleware({
-  // dependencies: { ajax: rxAjax },
+  dependencies: { rxAjax: ajax },
 });
