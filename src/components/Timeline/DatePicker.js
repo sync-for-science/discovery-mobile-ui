7861833d2d3a58741c0dbdd6246769560763c15a
erwin foxtree <foxtree@users.noreply.github.com>
2021-03-03 14:00:26 -0500

    fix Android

--- src/components/Timeline/DatePicker.js
+++ src/components/Timeline/DatePicker.js
@@ -1,31 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { string, func, instanceOf } from 'prop-types';
 import {
-  Text, SafeAreaView, StyleSheet,
+  Platform, SafeAreaView, Text, Button, StyleSheet,
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 const DatePicker = ({
   label, activeDate, minimumDate, maximumDate, onDateSelect,
 }) => {
+  const isAndroid = Platform.OS === 'android';
+
+  // by default, Android date picker is open:
+  const [isVisible, setVisible] = useState(!isAndroid);
+
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || activeDate;
-    // (Platform.OS === 'ios');
+    if (isAndroid) {
+      setVisible(false);
+    }
     onDateSelect(currentDate);
   };
 
   return (
     <SafeAreaView style={styles.container}>
       <Text>{label}</Text>
-      <DateTimePicker
-        style={styles.picker}
-        mode="date"
-        value={activeDate}
-        minimumDate={minimumDate}
-        maximumDate={maximumDate}
-        display="default"
-        onChange={onChange}
-      />
+      {
+        isAndroid && (
+          <Button
+            title="DATE HERE"
+            onPress={() => setVisible(true)}
+          />
+        )
+      }
+      {
+        isVisible && (
+          <DateTimePicker
+            style={styles.picker}
+            mode="date"
+            value={activeDate}
+            minimumDate={minimumDate}
+            maximumDate={maximumDate}
+            display="default"
+            onChange={onChange}
+          />
+        )
+      }
     </SafeAreaView>
   );
 };
