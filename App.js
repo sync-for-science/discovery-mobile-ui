2f3b2fb76a45e1ccdffa901f086d4251c7e232e7
Gilbert Hsu <gilbert@vermonster.com>
2021-03-03 14:45:53 -0500

    suppress warnings

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
 import store from './src/redux';
 import RootNavigator from './src/navigation/RootNavigator';
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['VirtualizedLists'])
 
 export default function App() {
   return (
