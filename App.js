0b8c111b409bff8b52dec6afdd197ebcb67f7a97
Gilbert Hsu <45667486+gilhsu@users.noreply.github.com>
2021-03-03 14:58:38 -0500

    Suppress VirtualizedList Warnings (#43)
    * suppress warnings
    
    * linting

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-
+import { LogBox } from 'react-native';
 import store from './src/redux';
 import RootNavigator from './src/navigation/RootNavigator';
+
+LogBox.ignoreLogs(['VirtualizedLists']);
 
 export default function App() {
   return (
