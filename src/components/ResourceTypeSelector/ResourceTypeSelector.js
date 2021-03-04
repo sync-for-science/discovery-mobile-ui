d1b6b238343d0699a2fc309d1d1d8122df806c21
Vadim Peretokin <vperetokin@gmail.com>
2021-03-04 14:24:48 +0100

    Fix buttons losing text after deselection

--- src/components/ResourceTypeSelector/ResourceTypeSelector.js
+++ src/components/ResourceTypeSelector/ResourceTypeSelector.js
@@ -15,7 +15,9 @@ import { selectResourceType } from '../../redux/epics';
 const CategoryButton = ({ resourceType, selectedResourceType, selectResourceTypeAction }) => {
   const categoryDisplay = RESOURCE_TYPES[resourceType];
   const buttonStyle = selectedResourceType === resourceType ? styles.buttonSelected : styles.button;
-  const buttonTextStyle = selectedResourceType === resourceType ? styles.buttonSelectedText : null;
+  const buttonTextStyle = selectedResourceType
+    === resourceType ? styles.buttonSelectedText : styles.buttonText;
+
   return (
     <Button style={buttonStyle} onPress={() => selectResourceTypeAction(resourceType)}>
       <Text style={buttonTextStyle}>{categoryDisplay}</Text>
@@ -98,5 +100,8 @@ const styles = StyleSheet.create({
   },
   buttonSelectedText: {
     color: 'white',
+  },
+  buttonText: {
+    color: 'black',
   },
 });
