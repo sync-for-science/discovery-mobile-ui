5cad44c86c44a8d3aecf54dec467f2ce93d7c0f5
Gilbert Hsu <gilbert@vermonster.com>
2021-03-22 14:55:56 -0400

    wip

---
+++ src/components/CollectionModal/NewCollectionModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Button } from 'react-native'
+
+const NewCollectionModal = () => {
+  return (
+    <Button title="New Collection Modal" />
+  )
+}
+
+export default NewCollectionModal
+
+const styles = StyleSheet.create({})
