c1ce65b86b4c159a3e6f9c5d2c88cc4e7ce86606
Gilbert Hsu <gilbert@vermonster.com>
2021-03-22 17:39:48 -0400

    build basic dialog

--- src/components/CollectionModal/NewCollectionModal.js
+++ src/components/CollectionModal/NewCollectionModal.js
@@ -1,13 +1,30 @@
 import React from 'react'
-import { StyleSheet, Button, Modal, View, Text, SafeAreaView } from 'react-native'
+import { StyleSheet, TextInput, View } from 'react-native'
+import Dialog from 'react-native-dialog'
+
+import Colors from '../../constants/Colors'
 
 const NewCollectionModal = ({modalVisible, setModalVisible}) => {
   return (
-    <SafeAreaView style={styles.base}>
-      <View >
-       <Text>NewCollectionModal</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.base}>
+      <Dialog.Container visible={modalVisible} keyboardVerticalOffset={250}>
+        <Dialog.Title>New Collection</Dialog.Title>
+        <Dialog.Description>
+          Enter name for this new collection.
+        </Dialog.Description>
+        <TextInput 
+          style={styles.textInput}
+          placeholder="Name"
+          autoFocus={true}
+        />
+        <Dialog.Button 
+          label="Cancel" 
+          color={Colors.darkgrey} 
+          onPress={() => setModalVisible(false)}
+        />
+        <Dialog.Button label="Create" bold />
+      </Dialog.Container>
+    </View>
   )
 }
 
@@ -15,10 +32,14 @@ export default NewCollectionModal
 
 const styles = StyleSheet.create({
   base: {
-    padding: 100,
-    height: 100,
-    width: 100,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1,
+  },
+  textInput: {
+    borderColor: Colors.darkgrey,
+    borderWidth: 1,
+    borderRadius: 5,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    padding: 5
   }
 })
