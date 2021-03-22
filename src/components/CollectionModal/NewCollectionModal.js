eddd267788ff11ac608700c8ac6472d261a1ea96
Gilbert Hsu <gilbert@vermonster.com>
2021-03-22 18:17:30 -0400

    create dialog handlers

--- src/components/CollectionModal/NewCollectionModal.js
+++ src/components/CollectionModal/NewCollectionModal.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { StyleSheet, TextInput, View } from 'react-native'
 import Dialog from 'react-native-dialog'
 
 import Colors from '../../constants/Colors'
 
 const NewCollectionModal = ({modalVisible, setModalVisible}) => {
+  const [name, setName] = useState("")
+
+  const handleSubmit = () => {
+    setName("")
+    setModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setName("")
+    setModalVisible(false)
+  }
+
   return (
     <View style={styles.base}>
-      <Dialog.Container visible={modalVisible} keyboardVerticalOffset={250}>
+      <Dialog.Container 
+        visible={modalVisible} 
+        keyboardVerticalOffset={250}
+        onBackdropPress={handleCancel}
+      >
         <Dialog.Title>New Collection</Dialog.Title>
         <Dialog.Description>
           Enter name for this new collection.
@@ -16,13 +32,21 @@ const NewCollectionModal = ({modalVisible, setModalVisible}) => {
           style={styles.textInput}
           placeholder="Name"
           autoFocus={true}
+          autoCorrect={false}
+          value={name}
+          onChangeText={(text) => setName(text)}
+          
         />
         <Dialog.Button 
           label="Cancel" 
           color={Colors.darkgrey} 
-          onPress={() => setModalVisible(false)}
+          onPress={handleCancel}
         />
-        <Dialog.Button label="Create" bold />
+        <Dialog.Button 
+          label="Create" 
+          bold 
+          onPress={handleSubmit}
+        />
       </Dialog.Container>
     </View>
   )
