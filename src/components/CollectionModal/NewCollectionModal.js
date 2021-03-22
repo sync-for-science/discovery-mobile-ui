7c5817f30f5436af19035bd41089ad2f7af79ef5
Gilbert Hsu <gilbert@vermonster.com>
2021-03-22 17:16:47 -0400

    wip

--- src/components/CollectionModal/NewCollectionModal.js
+++ src/components/CollectionModal/NewCollectionModal.js
@@ -1,12 +1,24 @@
 import React from 'react'
-import { StyleSheet, Button } from 'react-native'
+import { StyleSheet, Button, Modal, View, Text, SafeAreaView } from 'react-native'
 
-const NewCollectionModal = () => {
+const NewCollectionModal = ({modalVisible, setModalVisible}) => {
   return (
-    <Button title="New Collection Modal" />
+    <SafeAreaView style={styles.base}>
+      <View >
+       <Text>NewCollectionModal</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default NewCollectionModal
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  base: {
+    padding: 100,
+    height: 100,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
