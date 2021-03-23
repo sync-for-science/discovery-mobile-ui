185c326d42de0ab416e2a5d21af75aa5b06fd3aa
Gilbert Hsu <gilbert@vermonster.com>
2021-03-23 14:59:04 -0400

    add ActionSheetIOS

--- src/components/Icons/CollectionRowActionIcon.js
+++ src/components/Icons/CollectionRowActionIcon.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, TouchableOpacity, ActionSheetIOS, View } from 'react-native'
+import { Entypo } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
+
+import Colors from '../../constants/Colors';
 
 const CollectionRowActionIcon = () => {
+
+  const handlePress = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ["Cancel", "Edit Collection", "Delete Collection"],
+        destructiveButtonIndex: 2,
+        cancelButtonIndex: 0,
+        userInterfaceStyle: 'dark'
+      },
+      buttonIndex => {
+        if (buttonIndex === 0) {
+          // cancel action
+        } else if (buttonIndex === 1) {
+          setResult(Math.floor(Math.random() * 100) + 1);
+        } else if (buttonIndex === 2) {
+          setResult("🔮");
+        }
+      }
+    );
+  }
+
   return (
     <View>
-      <Text></Text>
+      <TouchableOpacity onPress={handlePress}>
+        <Entypo name="dots-three-horizontal" size={24} color={Colors.darkgrey} />
+      </TouchableOpacity>
     </View>
   )
 }
