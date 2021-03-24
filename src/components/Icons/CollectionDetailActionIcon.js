e113c1e3bb5239632930b5c99856947e4f6570b1
Gilbert Hsu <gilbert@vermonster.com>
2021-03-24 11:31:58 -0400

    refactor CollectionDetailActionIcon

--- src/components/Icons/CollectionDetailActionIcon.js
+++ src/components/Icons/CollectionDetailActionIcon.js
@@ -1,17 +1,110 @@
-import React from 'react'
-import { StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import React from 'react';
+import {
+  TouchableOpacity, ActionSheetIOS, View, Alert,
+} from 'react-native';
 import { Entypo } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
+import { connect } from 'react-redux';
+import {
+  bool, func, number, shape, string,
+} from 'prop-types';
 
-import Colors from '../../constants/Colors'
+import { deleteCollection, renameCollection } from '../../redux/action-creators';
+import { collectionsCountSelector } from '../../redux/selectors';
+import Colors from '../../constants/Colors';
 
-const CollectionDetailActionIcon = () => {
+const CoolectionDetailActionIcon = ({
+  collections,
+  collectionId,
+  collectionsCount,
+  deleteCollectionAction,
+  renameCollectionAction,
+  selected,
+}) => {
+  const handleDeleteCollection = () => {
+    const nextCollectionId = selected
+      ? Object.keys(collections).filter((id) => id !== collectionId)[0]
+      : null;
+    deleteCollectionAction(collectionId, nextCollectionId);
+  };
+  const handlePress = () => {
+    ActionSheetIOS.showActionSheetWithOptions(
+      {
+        options: ['Cancel', 'Rename Collection', 'Delete Collection'],
+        destructiveButtonIndex: 2,
+        cancelButtonIndex: 0,
+        userInterfaceStyle: 'dark',
+      },
+      (buttonIndex) => {
+        if (buttonIndex === 0) {
+          // cancel action
+        } else if (buttonIndex === 1) {
+          Alert.prompt(
+            'Rename Collection',
+            'Enter name for this new collection.',
+            [
+              {
+                text: 'Cancel',
+                onPress: () => {},
+                style: 'cancel',
+              },
+              {
+                text: 'Rename',
+                onPress: (text) => renameCollectionAction(collectionId, text),
+              },
+            ],
+          );
+        } else if (buttonIndex === 2) {
+          if (collectionsCount <= 1) {
+            Alert.alert('Delete Error', 'Cannot delete last collection.');
+          } else {
+            Alert.alert(
+              'Delete Collection',
+              'Are you sure you want to delete this collection?',
+              [
+                {
+                  text: 'Cancel',
+                  onPress: () => {},
+                  style: 'cancel',
+                },
+                {
+                  text: 'Delete',
+                  onPress: handleDeleteCollection,
+                  style: 'destructive',
+                },
+              ],
+            );
+          }
+        }
+      },
+    );
+  };
+
   return (
-    <TouchableOpacity onPress={() => Alert.alert('hello!')}>
-      <Entypo name="dots-three-horizontal" size={24} color={Colors.primary} />
-    </TouchableOpacity>
-  )
-}
+    <View>
+      <TouchableOpacity onPress={handlePress}>
+        <Entypo name="dots-three-horizontal" size={24} color={Colors.darkgrey} />
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-export default CollectionDetailActionIcon
+CoolectionDetailActionIcon.propTypes = {
+  collections: shape({}).isRequired,
+  collectionId: string.isRequired,
+  collectionsCount: number.isRequired,
+  deleteCollectionAction: func.isRequired,
+  renameCollectionAction: func.isRequired,
+  selected: bool.isRequired,
+};
 
-const styles = StyleSheet.create({})
+const mapStateToProps = (state) => ({
+  collectionsCount: collectionsCountSelector(state),
+  collections: state.collections,
+});
+
+const mapDispatchToProps = {
+  deleteCollectionAction: deleteCollection,
+  renameCollectionAction: renameCollection,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoolectionDetailActionIcon);
--- src/screens/CollectionDetailsScreen.js
+++ src/screens/CollectionDetailsScreen.js
@@ -9,28 +9,28 @@ import {
 } from 'native-base';
 import { Ionicons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
 
-
 import Colors from '../constants/Colors';
 import CollectionDetailActionIcon from '../components/Icons/CollectionDetailActionIcon'
 
-const CollectionsDetailsScreen = ({ route, navigation, collections }) => {
+const CollectionsDetailsScreen = ({ route, navigation, collections, selectedCollectionId }) => {
   const { params: { collectionId } } = route;
   const collection = collections[collectionId];
+  const selected = collectionId === selectedCollectionId;
 
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <StatusBar backgroundColor={Colors.primary} barStyle="dark-content" />
       <Header style={styles.header}>
-        <Left>
+        <Left >
           <TouchableOpacity onPress={() => navigation.navigate('CollectionsIndex')}>
             <Ionicons name="chevron-back" size={30} color={Colors.primary} />
           </TouchableOpacity>
         </Left>
-        <Body>
+        <View style={styles.headerTitle}>
           <Title>{collection.label}</Title>
-        </Body>
+        </View>
         <Right>
-          <CollectionDetailActionIcon />
+          <CollectionDetailActionIcon selected={selected} collectionId={collectionId} />
         </Right>
       </Header>
       <View style={styles.screen}>
@@ -50,6 +50,7 @@ CollectionsDetailsScreen.propTypes = {
 
 const mapStateToProps = (state) => ({
   collections: state.collections,
+  selectedCollectionId: state.selectedCollection,
 });
 
 export default connect(mapStateToProps, null)(CollectionsDetailsScreen);
@@ -65,9 +66,14 @@ const styles = StyleSheet.create({
   collectionContainer: {
     backgroundColor: 'white',
     padding: 20,
-    marginTop: 50
+    marginTop: 50,
+    width: '80%'
   },
   header: {
     backgroundColor: Colors.screenBackground,
+    alignItems: 'center'
   },
+  headerTitle: {
+    width: '70%'
+  }
 });
