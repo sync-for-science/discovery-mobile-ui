554ee4d185f2f5686ba978522ab18668438c1ceb
erwin foxtree <foxtree@users.noreply.github.com>
2021-04-08 15:13:13 -0400

    persistStoreFactory

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,6 +1,13 @@
 import { combineReducers, compose, configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+// import { AsyncStorage } from 'react-native';
+// import immutableTransform from 'redux-persist-transform-immutable';
+import { persistReducer, persistStore } from 'redux-persist';
+// autoRehydrate
+// import storage from 'redux-persist/lib/storage';
+// import hardSet from 'redux-persist/lib/stateReconciler/hardSet';
 
+import { AsyncStorage } from 'react-native';
 import authReducer from '../features/auth/authSlice';
 import {
   flattenedResourcesReducer,
@@ -14,22 +21,54 @@ const rootReducer = combineReducers({
   auth: authReducer,
   resources: flattenedResourcesReducer,
   resourceIdsGroupedByType: resourceTypesReducer,
-  collections: collectionsReducer,
   activeCollectionId: activeCollectionIdReducer,
+  collections: collectionsReducer,
 });
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: compose([
-    thunk,
-    epicMiddleware,
-    // routerMiddleware(history), // < e.g.: other middleware
-  ]),
-  devTools: {
-    serialize: true, // See: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md#serialize
-  },
-});
+export const persistReducerConfig = {
+  version: '0.1',
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['activeCollectionId', 'collections'],
+  // stateReconciler: hardSet,
+};
 
-epicMiddleware.run(rootEpic);
+const persistConfig = {
+};
 
-export default store;
+let persistor;
+export const getPersistor = () => persistor;
+
+const persistStoreFactory = () => {
+  const store = configureStore({
+    // reducer: rootReducer,
+    reducer: persistReducer(persistReducerConfig, rootReducer),
+    middleware: compose([
+      thunk,
+      epicMiddleware,
+      // routerMiddleware(history), // < e.g.: other middleware
+    ]),
+    devTools: {
+      serialize: true, // See: https://github.com/zalmoxisus/redux-devtools-extension/blob/master/docs/API/Arguments.md#serialize
+    },
+  });
+
+  epicMiddleware.run(rootEpic);
+
+  const callback = (result) => {
+    console.info('persistStore CALLBACK: ', result);
+  };
+
+  persistor = persistStore(store, persistConfig, callback);
+  // persistor.purge();
+  // persistor.flush();
+  // persistor.pause();
+  // persistor.persist();
+
+  return {
+    store,
+    persistor,
+  };
+};
+
+export default persistStoreFactory;
