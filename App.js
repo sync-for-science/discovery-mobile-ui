de5bde28148ed7fd94482f256aab752f655c2961
erwin foxtree <foxtree@users.noreply.github.com>
2021-04-08 15:11:39 -0400

    App.js

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { LogBox, View } from 'react-native';
 import * as Font from 'expo-font'; // eslint-disable-line import/no-extraneous-dependencies
 import { Ionicons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
 import Roboto from 'native-base/Fonts/Roboto.ttf';
+
 import Roboto_medium from 'native-base/Fonts/Roboto_medium.ttf'; // eslint-disable-line camelcase
-import store from './src/redux';
+import persistStoreFactory from './src/redux';
 import RootNavigator from './src/navigation/RootNavigator';
 
 LogBox.ignoreLogs(['VirtualizedLists']);
+
+const { store, persistor } = persistStoreFactory();
 
 export default function App() {
   const [loading, setLoading] = useState(false);
@@ -32,7 +36,9 @@ export default function App() {
 
   return (
     <Provider store={store}>
-      <RootNavigator />
+      <PersistGate loading={null} persistor={persistor}>
+        <RootNavigator />
+      </PersistGate>
     </Provider>
   );
 }
