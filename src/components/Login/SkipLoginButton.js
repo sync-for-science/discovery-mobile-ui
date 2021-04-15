9e1f1735d1edb358894c9bcec1b8c731ccb1dc8e
erwin foxtree <foxtree@users.noreply.github.com>
2021-04-15 11:24:51 -0400

    ensure MOCK_BUNDLE is set after MOCK_AUTH (#123)

--- src/components/Login/SkipLoginButton.js
+++ src/components/Login/SkipLoginButton.js
@@ -6,6 +6,8 @@ import { Button } from 'react-native';
 import { actionTypes } from '../../redux/action-types';
 import mockBundle from '../../../assets/mock_data/bundle-blake-eichmann.json';
 
+export const MOCK_BUNDLE = mockBundle;
+
 export const MOCK_AUTH = {
   baseUrl: '/', // some value is needed to successfully instantiate fhir-kit-client, to resolve contained resources
   authResult: {
@@ -18,12 +20,12 @@ export const MOCK_AUTH = {
 
 const showSkipLogin = process.env.NODE_ENV === 'development';
 
-const SkipLoginButton = ({ setMockData }) => {
+const SkipLoginButton = ({ setMockAuth }) => {
   if (showSkipLogin) {
     return (
       <Button
         title="Skip Login"
-        onPress={setMockData}
+        onPress={setMockAuth}
       />
     );
   }
@@ -31,20 +33,14 @@ const SkipLoginButton = ({ setMockData }) => {
 };
 
 SkipLoginButton.propTypes = {
-  setMockData: PropTypes.func.isRequired,
+  setMockAuth: PropTypes.func.isRequired,
 };
 
-const mapDispatchToProps = (dispatch) => ({
-  setMockData: () => {
-    dispatch(({
-      type: actionTypes.SET_AUTH,
-      payload: MOCK_AUTH,
-    }));
-    dispatch(({
-      type: actionTypes.FHIR_FETCH_SUCCESS,
-      payload: mockBundle,
-    }));
-  },
-});
+const mapDispatchToProps = {
+  setMockAuth: () => ({
+    type: actionTypes.SET_AUTH,
+    payload: MOCK_AUTH,
+  }),
+};
 
 export default connect(null, mapDispatchToProps)(SkipLoginButton);
