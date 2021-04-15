b91e513f605a8fed72c670079307656a4a2f737e
Gilbert Hsu <gilbert@vermonster.com>
2021-04-15 11:28:48 -0400

    pass disabled props from CatalogModal

--- src/components/SegmentControl/CollectionSegmentControl.js
+++ src/components/SegmentControl/CollectionSegmentControl.js
@@ -6,17 +6,15 @@ import BaseSegmentControl from '../Generic/BaseSegmentControl';
 
 import BaseText from '../Generic/BaseText';
 import { toggleShowCollectionOnly } from '../../redux/action-creators';
-import { activeCollectionSelector } from '../../redux/selectors';
 
 const allRecordsDescription = 'Displays all records.';
 const collectionRecordsDescription = 'Only displays records saved to the collection.';
 
 const CollectionSegmentControl = ({
-  collection,
+  hasResourceIds,
+  showCollectionOnly,
   toggleShowCollectionOnlyAction,
 }) => {
-  const { resourceIds, showCollectionOnly } = collection
-  const hasResourceIds = Object.keys(resourceIds).length > 0
   const segControlIndex = showCollectionOnly ? 1 : 0;
   const description = segControlIndex === 0 ? allRecordsDescription : collectionRecordsDescription;
   const handleChange = (selectedSegmentIndex) => {
@@ -40,15 +38,11 @@ CollectionSegmentControl.propTypes = {
   toggleShowCollectionOnlyAction: func.isRequired,
 };
 
-const mapStateToProps = (state) => ({
-  collection: activeCollectionSelector(state),
-});
-
 const mapDispatchToProps = {
   toggleShowCollectionOnlyAction: toggleShowCollectionOnly,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(CollectionSegmentControl);
+export default connect(null, mapDispatchToProps)(CollectionSegmentControl);
 
 const styles = StyleSheet.create({
   root: {
