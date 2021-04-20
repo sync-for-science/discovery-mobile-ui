7544aef5a9cab09746fd7c0afe988dedd0afc9ae
Gilbert Hsu <gilbert@vermonster.com>
2021-04-20 13:08:08 -0400

    wip render accordions

--- src/components/DateAccordion/DateAccordion.js
+++ src/components/DateAccordion/DateAccordion.js
@@ -1,22 +1,88 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { StyleSheet, Text, View } from 'react-native'
-import { activeCollectionSelector } from '../../redux/selectors'
+import { StyleSheet, View } from 'react-native'
+import { Accordion } from 'native-base';
+import { activeCollectionIdSelector } from '../../redux/selectors'
+import { Ionicons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
 
-const DateAccordion = ({label, resourceIds, collection}) => {
-  const collectionResourceIds = collection.resourceIds
+import Colors from '../../constants/Colors';
+import ResourceCard from '../ResourceCard/ResourceCard';
+import BaseText from '../Generic/BaseText';
+import CountIcon from '../Icons/CountIcon';
+
+const DateAccordion = ({label, resourceIds, activeCollectionId}) => {
+  const dataArray = [{ title: label, content: resourceIds }];
+
+  const renderHeader = (item, expanded) => {
+    const chevronIcon = expanded
+      ? <Ionicons name="chevron-down" size={24} color={Colors.accordionChevronIcon} />
+      : <Ionicons name="chevron-up" size={24} color={Colors.accordionChevronIcon} />;
+
+    return (
+      <View style={styles.header}>
+        <View style={styles.headerTextContainer}>
+          {chevronIcon}
+          <CountIcon count={0} />
+          <BaseText style={styles.headerText}>
+            {item.title}
+          </BaseText>
+        </View>
+      </View>
+    );
+  };
+
+  const renderContent = (item) => item.content.map(
+    (resourceId, cardIndex) => (
+      <ResourceCard
+        key={resourceId}
+        index={cardIndex}
+        resourceId={resourceId}
+        collectionId={activeCollectionId}
+      />
+    ),
+  );
+
   return (
-    <View>
-      <Text>{label}</Text>
-      {resourceIds.map(resourceId => <Text>{collectionResourceIds[resourceId].toString()}</Text>)}
+    <View style={styles.accordionContainer}>
+      <Accordion
+        style={styles.accordion}
+        dataArray={dataArray}
+        expanded={[]}
+        renderHeader={renderHeader}
+        renderContent={renderContent}
+      />
     </View>
-  )
+  );
 }
 
 const mapStateToProps = (state) => ({
-  collection: activeCollectionSelector(state)
+  activeCollectionId: activeCollectionIdSelector(state)
 })
 
 export default connect(mapStateToProps, null)(DateAccordion)
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    padding: 5,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  headerTextContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1,
+  },
+  headerText: {
+    marginLeft: 5,
+    color: 'black',
+    flex: 1,
+  },
+  accordionContainer: {
+    backgroundColor: Colors.resourceCardBorder,
+  },
+  accordion: {
+    borderWidth: 0,
+  },
+});
