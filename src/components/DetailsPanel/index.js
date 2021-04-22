a418288879d2b9c2c1240557f611a39146682004
Gilbert Hsu <gilbert@vermonster.com>
2021-04-22 10:31:04 -0400

    remove ActionSheets from DetailsPanel

--- src/components/DetailsPanel/index.js
+++ src/components/DetailsPanel/index.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet, View, ActionSheetIOS, SafeAreaView,
+  StyleSheet, View, SafeAreaView,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
 import {
   Header, Right, Title, Left,
 } from 'native-base';
-import { Entypo, SimpleLineIcons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
+import { SimpleLineIcons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
 import { shape } from 'prop-types';
 
 import { timeSavedSortedCollectionResourceIdsSelector, recordDateSortedCollectionResourceIdsSelector } from '../../redux/selectors';
@@ -34,25 +34,6 @@ const DetailsPanel = ({
     },
   };
   const [sortingState, setSortingState] = useState(defaultSortingState);
-
-  const handlePressSortIcon = () => {
-    ActionSheetIOS.showActionSheetWithOptions(
-      {
-        options: ['Cancel', 'Sort Records By Category', 'Sort Records By Date Saved'],
-        cancelButtonIndex: 0,
-        userInterfaceStyle: 'dark',
-      },
-      (buttonIndex) => {
-        if (buttonIndex === 0) {
-          // cancel action
-        } else if (buttonIndex === 1) {
-          // Sort Records By Category;
-        } else if (buttonIndex === 2) {
-          // Sort Records By Date Saved;
-        }
-      },
-    );
-  };
 
   const handlePressNoteIcon = () => {
     navigation.navigate('CollectionNotes');
@@ -93,17 +74,13 @@ const DetailsPanel = ({
           <Title>{collection?.label}</Title>
         </View>
         <Right>
-          <TouchableOpacity style={styles.noteIcon} onPress={handlePressNoteIcon}>
+          <TouchableOpacity onPress={handlePressNoteIcon}>
             <SimpleLineIcons name="note" size={20} color={Colors.headerIcon} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={handlePressSortIcon}>
-            <Entypo name="dots-three-vertical" size={20} color={Colors.headerIcon} />
           </TouchableOpacity>
         </Right>
       </Header>
       <SortingHeader sortingState={sortingState} setSortingState={setSortingState} />
       <ScrollView>
-
         {displayAccordions}
       </ScrollView>
     </SafeAreaView>
@@ -132,8 +109,5 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     alignItems: 'center',
     elevation: 0,
-  },
-  noteIcon: {
-    marginRight: 15,
   },
 });
