cf04225c480f6c05b023a0b105dc80290b07da76
Gilbert Hsu <gilbert@vermonster.com>
2021-04-26 15:32:29 -0400

    refactor SubTypeAccordionsContainer to handle Type header

--- src/components/SubTypeAccordion/SubTypeAccordionsContainer.js
+++ src/components/SubTypeAccordion/SubTypeAccordionsContainer.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import {
-  StyleSheet, View,
+  StyleSheet, View
 } from 'react-native';
 import { shape, arrayOf, bool } from 'prop-types';
 
 import SubTypeAccordion from './SubTypeAccordion';
+import BaseText from '../Generic/BaseText'
 
-const SubTypeAccordionsContainer = ({ data, fromDetailsPanel }) => (
-  <View style={styles.root}>
-    <View style={styles.container}>
-      {data.map(({ subType, recordIds }) => (
-        <SubTypeAccordion
-          key={subType}
-          subType={subType}
-          resourceIds={recordIds}
-          subTypeCount={recordIds.length}
-          fromDetailsPanel={fromDetailsPanel}
-        />
-      ))}
+const SubTypeAccordionsContainer = ({ data, fromDetailsPanel }) => {
+  return (
+    <View>
+      { data.map(({type, label, subTypes}, index) => {
+          const firstGroupStyle = index === 0 ? styles.firstGroupContainer : {};
+          return (
+            <View key={type} style={[styles.groupContainer, firstGroupStyle]}>
+             {fromDetailsPanel && 
+                <View style={styles.typeTextContainer}>
+                  <BaseText variant="title" style={styles.typeText}>
+                    {label}
+                  </BaseText>
+                </View>
+              }
+              <View style={styles.root}>
+                <View style={styles.container}>
+                  {subTypes.map(({ subType, recordIds }) => (
+                    <SubTypeAccordion
+                      key={subType}
+                      subType={subType}
+                      resourceIds={recordIds}
+                      subTypeCount={recordIds.length}
+                      fromDetailsPanel={fromDetailsPanel}
+                    />
+                  ))}
+                </View>
+              </View>
+            </View>
+          );
+        })
+      }
     </View>
-  </View>
-);
+  )
+};
 
 SubTypeAccordionsContainer.propTypes = {
   data: arrayOf(shape({})).isRequired,
@@ -41,5 +61,17 @@ const styles = StyleSheet.create({
   },
   container: {
     width: '100%',
+  },
+  groupContainer: {
+    marginTop: 20,
+  },
+  firstGroupContainer: {
+    marginTop: 0,
+  },
+  typeTextContainer: {
+    marginLeft: 5,
+  },
+  typeText: {
+    textTransform: 'uppercase',
   },
 });
