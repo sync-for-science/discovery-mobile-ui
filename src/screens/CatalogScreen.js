9503d7868d17647e0fc23d63b7ae2141ea77f9d3
erwin foxtree <foxtree@users.noreply.github.com>
2021-04-29 13:17:39 -0400

    remove unused "screen" style

--- src/screens/CatalogScreen.js
+++ src/screens/CatalogScreen.js
@@ -37,9 +37,4 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
   },
-  screen: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
 });
