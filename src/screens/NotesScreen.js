8419525ab37b993ed569ed9b4434b8752789c417
Gilbert Hsu <gilbert@vermonster.com>
2021-05-04 14:58:04 -0400

    start record-notes wip

--- src/screens/NotesScreen.js
+++ src/screens/NotesScreen.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
-  SafeAreaView, StyleSheet, View, TouchableOpacity, ScrollView,
+  SafeAreaView, StyleSheet, View, TouchableOpacity, ScrollView, TextInput, KeyboardAvoidingView,
 } from 'react-native';
 import {
   Header, Right, Title, Left,
@@ -16,6 +16,7 @@ import ResourceCard from '../components/ResourceCard/ResourceCard';
 
 const NotesScreen = ({ resource }) => {
   const navigation = useNavigation();
+  const [text, onChangeText] = useState('')
   return (
     <SafeAreaView style={styles.root}>
       <Header style={styles.header}>
@@ -36,6 +37,22 @@ const NotesScreen = ({ resource }) => {
       <ScrollView>
         <ResourceCard resourceId={resource.id} resource={resource} fromNotesScreen />
       </ScrollView>
+      <KeyboardAvoidingView behavior="position">
+        <View style={{padding: 10, backgroundColor: Colors.lightgrey, flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
+          <TextInput
+            style={{
+              backgroundColor: 'white', 
+              flex: 1,  
+              borderRadius: 10,
+              padding: 10,
+              height: 100
+            }} 
+            // autoFocus={enableShift}
+            onChangeText={onChangeText}
+            numberOfLines={10}
+          />
+        </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
