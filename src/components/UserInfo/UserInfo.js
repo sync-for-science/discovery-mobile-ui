231d76f0bbd62fb106df5d2b44642b400cc17db9
erwin foxtree <foxtree@users.noreply.github.com>
2021-05-06 10:46:44 -0400

    remove "data range" from UserProfile

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,28 +1,20 @@
 import React from 'react';
-import { instanceOf, shape } from 'prop-types';
+import { shape } from 'prop-types';
 import { connect } from 'react-redux';
 import {
   StyleSheet, Text, View,
 } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
-import { format } from 'date-fns';
 
 import {
   getPatientName,
 } from '../../resources/fhirReader';
-import { patientSelector, dateRangeForAllRecordsSelector } from '../../redux/selectors';
-
-const UI_DATE_FORMAT = 'MMM d, Y';
-
-const formatDate = (d) => format(d, UI_DATE_FORMAT);
+import { patientSelector } from '../../redux/selectors';
 
 const UserInfo = ({
-  patientResource, timelineProps,
+  patientResource,
 }) => {
   const name = getPatientName(patientResource);
-  const { minimumDate, maximumDate } = timelineProps;
-  const dateRangeLabel = !(minimumDate && maximumDate) ? '' : 'Data range';
-  const dateRange = !(minimumDate && maximumDate) ? '' : `${formatDate(minimumDate)} - ${formatDate(maximumDate)}`;
 
   return (
     <View>
@@ -32,22 +24,12 @@ const UserInfo = ({
           {name}
         </Text>
       </View>
-      <View style={styles.dataRange}>
-        <Text style={styles.dataRangeLabel}>{dateRangeLabel}</Text>
-        <Text style={styles.dataRangeValue}>
-          {dateRange}
-        </Text>
-      </View>
     </View>
   );
 };
 
 UserInfo.propTypes = {
   patientResource: shape({}),
-  timelineProps: shape({
-    minimumDate: instanceOf(Date),
-    maximumDate: instanceOf(Date),
-  }).isRequired,
 };
 
 UserInfo.defaultProps = {
@@ -56,7 +38,6 @@ UserInfo.defaultProps = {
 
 const mapStateToProps = (state) => ({
   patientResource: patientSelector(state),
-  timelineProps: dateRangeForAllRecordsSelector(state),
 });
 
 export default connect(mapStateToProps, null)(UserInfo);
@@ -71,18 +52,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     padding: 15,
     paddingTop: 20,
-  },
-  userIcon: {
-  },
-  dataRange: {
-    flexDirection: 'row',
-  },
-  dataRangeLabel: {
-    textTransform: 'uppercase',
-    color: 'grey',
-    padding: 10,
-  },
-  dataRangeValue: {
-    padding: 10,
   },
 });
