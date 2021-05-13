35cd1a696d05c515ab545ec31e1fae1957b31da5
erwin foxtree <foxtree@users.noreply.github.com>
2021-05-13 13:29:48 -0400

    use DataRow for Providers tab

--- src/components/Summary/ProvidersSummary.js
+++ src/components/Summary/ProvidersSummary.js
@@ -4,38 +4,20 @@ import {
   arrayOf, shape, string,
 } from 'prop-types';
 import {
-  StyleSheet, Text, View,
+  StyleSheet, View,
 } from 'react-native';
 
 import { ScrollView } from 'react-native-gesture-handler';
 import { format } from 'date-fns';
 import { providersSelector, allResourcesByProviderSelector } from '../../redux/selectors';
 import RecordCount from './RecordCount';
-import Colors from '../../constants/Colors';
+import DataRow from './DataRow';
 
 const formatDate = (date) => {
   if (date) {
     return format(date, 'MMM d, Y');
   }
   return '';
-};
-
-const ProviderRow = ({ label, items }) => {
-  const oldest = items[0];
-  const latest = items.slice(-1)[0];
-  return (
-    <View style={styles.resourceTypeRow}>
-      <Text style={styles.resourceCount}>{items.length}</Text>
-      <Text style={styles.resourceLabel}>{label}</Text>
-      <Text style={styles.resourceDate}>{formatDate(oldest.timelineDate)}</Text>
-      <Text style={styles.resourceDate}>{formatDate(latest.timelineDate)}</Text>
-    </View>
-  );
-};
-
-ProviderRow.propTypes = {
-  label: string.isRequired,
-  items: arrayOf(shape({})).isRequired,
 };
 
 const ProvidersSummary = ({ providers, allResourcesByProvider }) => (
@@ -45,19 +27,25 @@ const ProvidersSummary = ({ providers, allResourcesByProvider }) => (
     />
     <ScrollView style={styles.scrollContainer}>
       <View style={styles.resourceTypeContainer}>
-        <View style={styles.resourceTypeRow}>
-          <Text style={styles.resourceCount} />
-          <Text style={styles.resourceLabel} />
-          <Text style={[styles.resourceDate, styles.tableHeading]}>Oldest</Text>
-          <Text style={[styles.resourceDate, styles.tableHeading]}>Latest</Text>
-        </View>
-        {providers.map(({ id, name }) => (
-          <ProviderRow
-            key={name}
-            label={name}
-            items={allResourcesByProvider[id]}
-          />
-        ))}
+        <DataRow
+          isHeadingRow
+          label=""
+          count=""
+          oldest="Oldest"
+          latest="Latest"
+        />
+        {providers.map(({ id, name }) => {
+          const items = allResourcesByProvider[id];
+          return (
+            <DataRow
+              key={name}
+              label={name}
+              count={items.length}
+              oldest={formatDate(items[0]?.timelineDate)}
+              latest={formatDate(items[items.length - 1]?.timelineDate)}
+            />
+          );
+        })}
       </View>
     </ScrollView>
   </View>
@@ -91,38 +79,5 @@ const styles = StyleSheet.create({
   resourceTypeContainer: {
     alignItems: 'center',
     width: '100%',
-  },
-  resourceTypeRow: {
-    width: '100%',
-    flex: 1,
-    flexDirection: 'row',
-    paddingVertical: 10,
-    paddingHorizontal: 5,
-    padding: 10,
-    backgroundColor: 'white',
-    borderBottomColor: 'lightgray',
-    borderBottomWidth: 1,
-
-  },
-  tableHeading: {
-    color: Colors.secondary,
-    fontSize: 12,
-    alignSelf: 'flex-end',
-  },
-  resourceCount: {
-    alignSelf: 'flex-start',
-    textAlign: 'right',
-    paddingRight: 10,
-    flex: 1,
-  },
-  resourceLabel: {
-    alignSelf: 'flex-start',
-    fontSize: 12,
-    flex: 4,
-  },
-  resourceDate: {
-    alignSelf: 'flex-end',
-    fontSize: 11,
-    flex: 2,
   },
 });
