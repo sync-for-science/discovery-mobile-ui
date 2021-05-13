fb0abcc693ff6ce2ff62e842c799aace01fff077
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 16:03:36 -0400

    style onboarding

--- src/components/Onboarding/Page1.js
+++ src/components/Onboarding/Page1.js
@@ -8,18 +8,26 @@ import { actionTypes } from '../../redux/action-types';
 const Page1 = () => {
   const dispatch = useDispatch();
   return (
-    <View>
+    <View style={styles.root}>
       <Text>Page1</Text>
-      <Button
-        onPress={() => dispatch({ type: actionTypes.COMPLETE_ONBOARDING })}
-        style={{ paddingHorizontal: 20 }}
-      >
-        <Text>SKIP ONBOARDING</Text>
-      </Button>
+      <View>
+        <Button
+          onPress={() => dispatch({ type: actionTypes.COMPLETE_ONBOARDING })}
+          style={{ paddingHorizontal: 20 }}
+        >
+          <Text>SKIP ONBOARDING</Text>
+        </Button>
+      </View>
     </View>
   );
 };
 
 export default Page1;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
