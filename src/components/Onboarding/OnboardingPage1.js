d00c85d4f2362b6bfd841c9c5c87d01c54a3f69d
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 16:49:23 -0400

    wip page1 styling

--- src/components/Onboarding/OnboardingPage1.js
+++ src/components/Onboarding/OnboardingPage1.js
@@ -5,12 +5,33 @@ import { useDispatch } from 'react-redux';
 
 import { actionTypes } from '../../redux/action-types';
 import TextStyles from '../../resources/textStyles'
+import Colors from '../../constants/Colors'
 
 const OnboardingPage1 = () => {
   const dispatch = useDispatch();
   return (
     <View style={styles.root}>
-      <Text style={TextStyles.h1}>Page1</Text>
+      <Text style={TextStyles.h1}>Welcome to Discovery</Text>
+      <View style={{height: 50, backgroundColor: Colors.mediumgrey, margin: 20}}>
+        <Text style={[TextStyles.h2, {fontStyle: 'italic', color: Colors.lightgrey}]}>LOGO HERE</Text>
+      </View>
+      <View style={{margin: 20}}>
+        <Text style={[TextStyles.h4, TextStyles.alignCenter]}>Use Discover to explore your personal medical data</Text>
+      </View>
+      <View style={{margin: 20}}>
+        <View>
+          <Text style={[TextStyles.h5, TextStyles.alignCenter]}>ACCESS</Text>
+        </View>
+        <View>
+          <Text style={[TextStyles.h5, TextStyles.alignCenter]}>ORGANIZE</Text>
+        </View>
+        <View>
+          <Text style={[TextStyles.h5, TextStyles.alignCenter]}>EXPLORE</Text>
+        </View>
+        <View>
+          <Text style={[TextStyles.h5, TextStyles.alignCenter]}>USE</Text>
+        </View>
+      </View>
       <View>
         <Button
           onPress={() => dispatch({ type: actionTypes.COMPLETE_ONBOARDING })}
@@ -30,5 +51,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    margin: 20
   },
 });
