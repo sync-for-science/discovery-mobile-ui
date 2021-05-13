5a4600e7359a2740656621f834a5376b94903ed6
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 17:42:43 -0400

    OnboardingHeader

---
+++ src/screens/Onboarding/OnboardingHeader.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const OnboardingHeader = () => {
+  return (
+    <View>
+      <Text>OnboardingHeader</Text>
+    </View>
+  )
+}
+
+export default OnboardingHeader
+
+const styles = StyleSheet.create({})
