f9d6ac16c3d312230bf4a04104911bd0b716c24d
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 16:03:36 -0400

    style onboarding

--- src/screens/OnboardingScreen.js
+++ src/screens/OnboardingScreen.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import Swiper from 'react-native-swiper';
 
 import Page1 from '../components/Onboarding/Page1';
 import Page2 from '../components/Onboarding/Page2';
 
 const OnboardingScreen = () => (
-  <SafeAreaView>
-    <Page1 />
-    <Page2 />
+  <SafeAreaView style={styles.root}>
+    <Swiper
+      loop={false}
+      index={0}
+    >
+
+      <Page1 />
+      <Page2 />
+    </Swiper>
   </SafeAreaView>
 );
 
 export default OnboardingScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
