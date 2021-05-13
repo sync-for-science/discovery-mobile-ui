aa3a7f2f51f87e052f900e5796f10c2cbc569d54
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 14:17:21 -0400

    create OnboardingNavigator and screen

---
+++ src/components/Onboarding/Page1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Page1 = () => {
+  return (
+    <View>
+      <Text>Page1</Text>
+    </View>
+  )
+}
+
+export default Page1
+
+const styles = StyleSheet.create({})
---
+++ src/components/Onboarding/Page2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Page2 = () => {
+  return (
+    <View>
+      <Text>Page2</Text>
+    </View>
+  )
+}
+
+export default Page2
+
+const styles = StyleSheet.create({})
