efe8e2ae72cc97e65ff9de0331f9b77a0a2f02a7
Gilbert Hsu <gilbert@vermonster.com>
2021-05-13 16:29:40 -0400

    create TextStyles

---
+++ src/resources/textStyles.js
@@ -0,0 +1,42 @@
+import Colors from "../constants/Colors";
+
+export default {
+  h1: {
+    fontSize: 36,
+    fontWeight: '700'
+  },
+  h2: {
+    fontSize: 32,
+    fontWeight: '700'
+  },
+  h3: {
+    fontSize: 28,
+    fontWeight: '700'
+  },
+  h4: {
+    fontSize: 24,
+    fontWeight: '600'
+  },
+  h5: {
+    fontSize: 20,
+    fontWeight: '600'
+  },
+  h6: {
+    fontSize: 16,
+    fontWeight: '600'
+  },
+  subtitle1: {
+    fontWeight: '600'
+  },
+  subtitle2: {
+    fontStyle: 'italic'
+  },
+  body1: {
+  },
+  body2: {
+    fontSize: 12,
+  },
+  uppercase: {
+    textTranform: 'uppercase' 
+  }
+}
