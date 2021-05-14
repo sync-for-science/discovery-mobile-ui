35ceb48c9667ba5b1a484326b22e6e6d6075a3cd
Gilbert Hsu <gilbert@vermonster.com>
2021-05-14 14:27:46 -0400

    skipOnboarding in OBNavigation

--- src/screens/Onboarding/OBHeader.js
+++ src/screens/Onboarding/OBHeader.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 const OBHeader = () => (
-  <View>
-    <Text>OBHeader</Text>
-  </View>
+  <View />
 );
 
 export default OBHeader;
