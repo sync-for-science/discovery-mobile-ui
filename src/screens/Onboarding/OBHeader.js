880da696562792b1cefee74c339f4d0456be503b
Gilbert Hsu <gilbert@vermonster.com>
2021-05-14 16:44:12 -0400

    style OBHeader

--- src/screens/Onboarding/OBHeader.js
+++ src/screens/Onboarding/OBHeader.js
@@ -1,10 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Colors from '../../constants/Colors';
+import TextStyles from '../../constants/TextStyles';
 
-const OBHeader = () => (
-  <View />
-);
+const TOTAL_OB_SCREEN_COUNT = 20
+
+const OBHeader = ({screenNumber}) => {
+  const { h3, h6, alignCenter, italic } = TextStyles
+  let filledMarkers = []
+  for(let i=0; i < screenNumber; i++) {
+    filledMarkers.push(<View style={{flex: 1, borderWidth: 1, borderColor: Colors.lightgrey2, backgroundColor: Colors.primary}} />)
+  }
+  let emptyMakers = []
+  for(let i=0; i < TOTAL_OB_SCREEN_COUNT - screenNumber; i++) {
+    emptyMakers.push(<View style={{flex: 1, borderWidth: 1, borderColor: Colors.lightgrey2}} />)
+  }
+
+  return (
+  <View style={styles.root}>
+    <View style={styles.progressContainer}>
+      {filledMarkers}
+      {emptyMakers}
+    </View>
+    <View style={styles.logoContainer}>
+      <View style={styles.iconContainer}>
+        <Text style={[h6, alignCenter, italic, styles.tempLogoText]}>LOGO HERE</Text>
+      </View>
+      <Text style={h3}>Logo Here</Text>
+    </View>
+  </View>
+)};
 
 export default OBHeader;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  progressContainer: {
+    flexDirection: 'row', 
+    height: 10
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    marginTop: 20,
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  iconContainer: {
+    height: 75,
+    width: 75,
+    backgroundColor: Colors.mediumgrey,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  tempLogoText: {
+    color: Colors.lightgrey,
+  },
+});
