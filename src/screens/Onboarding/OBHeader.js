11ec0c824ae1172f07484b4fc6d492514815a653
Gilbert Hsu <gilbert@vermonster.com>
2021-05-14 09:46:53 -0400

    rename files

---
+++ src/screens/Onboarding/OBHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const OBHeader = () => (
+  <View>
+    <Text>OBHeader</Text>
+  </View>
+);
+
+export default OBHeader;
+
+const styles = StyleSheet.create({});
