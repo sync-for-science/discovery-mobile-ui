828dfb110455571e5f645b28e526ba82f3ada9d1
Gilbert Hsu <gilbert@vermonster.com>
2021-05-17 11:56:01 -0400

    create OBScreenInfo

--- src/navigation/OnboardingNavigator.js
+++ src/navigation/OnboardingNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OBScreenWelcome from '../screens/Onboarding/OBScreenWelcome';
 import OBScreenSecurity from '../screens/Onboarding/DataAccess/OBScreenSecurity';
+import OBScreenInfo from '../screens/Onboarding/DataAccess/OBScreenInfo';
 
 const OnboardingStack = createStackNavigator();
 
@@ -13,6 +14,7 @@ const OnboardingNavigator = () => (
   >
     <OnboardingStack.Screen name="Welcome" component={OBScreenWelcome} />
     <OnboardingStack.Screen name="Security" component={OBScreenSecurity} />
+    <OnboardingStack.Screen name="Info" component={OBScreenInfo} />
   </OnboardingStack.Navigator>
 );
 
