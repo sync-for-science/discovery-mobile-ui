9bf3a6e99595906760a3fcf2c158f1e5fc75c051
Gilbert Hsu <gilbert@vermonster.com>
2021-05-17 16:09:42 -0400

    OBScreenSummary

--- src/screens/Onboarding/DataExploration/OBScreenUpdates.js
+++ src/screens/Onboarding/DataExploration/OBScreenUpdates.js
@@ -4,10 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 import OBTemplate from '../OBTemplate';
 import TextStyles from '../../../constants/TextStyles';
 
-// wireframe page 14
-const SCREEN_NUMBER = 12;
+// wireframe page 15
+const SCREEN_NUMBER = 13;
 const SECTION_TITLE = 'Data Exploration';
-const NEXT_SCREEN = 'Updates';
+const NEXT_SCREEN = 'Summary';
 
 const OBSCreenDisplaying = () => {
   const {
@@ -20,10 +20,10 @@ const OBSCreenDisplaying = () => {
       nextScreen={NEXT_SCREEN}
     >
       <View style={styles.default}>
-        <Text style={[h5, mb5]}>Summary</Text>
+        <Text style={[h5, mb5]}>Updates</Text>
         <Text style={[body1, alignCenter, mb5]}>
-          You will be able to see a high level Summary of your data,
-          organized by Record Types, and Providers and Practitioners.
+          You will be able to see what is latest in your data by selecting a
+          corresponding automatically generated Collection and start exploring from there.
         </Text>
       </View>
     </OBTemplate>
