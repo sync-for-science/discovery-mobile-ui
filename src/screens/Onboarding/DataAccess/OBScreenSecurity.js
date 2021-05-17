e509759b5d0075c05f74d6168d98f64c79b3e1fc
Gilbert Hsu <gilbert@vermonster.com>
2021-05-17 12:01:51 -0400

    refactor OBScreenInfo styles

--- src/screens/Onboarding/DataAccess/OBScreenSecurity.js
+++ src/screens/Onboarding/DataAccess/OBScreenSecurity.js
@@ -9,22 +9,22 @@ import Colors from '../../../constants/Colors';
 
 const SCREEN_NUMBER = 2;
 const SECTION_TITLE = 'Data Access';
-const NEXT_SCREEN = "Info"
+const NEXT_SCREEN = 'Info';
 
 // wireframe page 4
 const OBScreenSecurity = () => {
   const {
-    h4, h6, alignCenter, mb5,
+    h5, h6, body1, alignCenter, mb5,
   } = TextStyles;
   return (
     <OBTemplate screenNumber={SCREEN_NUMBER} sectionTitle={SECTION_TITLE} nextScreen={NEXT_SCREEN}>
       <View style={styles.default}>
-        <Text style={[h4, mb5]}>Security</Text>
-        <Text style={[h6, alignCenter, mb5]}>
+        <Text style={[h5, mb5]}>Security</Text>
+        <Text style={[body1, alignCenter, mb5]}>
           You will access your real medical data using Discovery in t
           he safest manner possible.
         </Text>
-        <Text style={[h6, alignCenter, mb5]}>
+        <Text style={[body1, alignCenter, mb5]}>
           We will never see or share your data. Encrypted medical
           data may be stored only on your phone, visible to only you
           and nobody else.
