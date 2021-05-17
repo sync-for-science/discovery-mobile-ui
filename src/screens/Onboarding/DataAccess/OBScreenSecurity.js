bf719f65dea062edbf67d1b65732aed1929a0f53
Gilbert Hsu <gilbert@vermonster.com>
2021-05-17 11:56:01 -0400

    create OBScreenInfo

--- src/screens/Onboarding/DataAccess/OBScreenSecurity.js
+++ src/screens/Onboarding/DataAccess/OBScreenSecurity.js
@@ -9,27 +9,28 @@ import Colors from '../../../constants/Colors';
 
 const SCREEN_NUMBER = 2;
 const SECTION_TITLE = 'Data Access';
+const NEXT_SCREEN = "Info"
 
 // wireframe page 4
 const OBScreenSecurity = () => {
   const {
-    h3, h5, alignCenter, mb5,
+    h4, h6, alignCenter, mb5,
   } = TextStyles;
   return (
-    <OBTemplate screenNumber={SCREEN_NUMBER} sectionTitle={SECTION_TITLE}>
+    <OBTemplate screenNumber={SCREEN_NUMBER} sectionTitle={SECTION_TITLE} nextScreen={NEXT_SCREEN}>
       <View style={styles.default}>
-        <Text style={[h3, mb5]}>Security</Text>
-        <Text style={[h5, alignCenter, mb5]}>
+        <Text style={[h4, mb5]}>Security</Text>
+        <Text style={[h6, alignCenter, mb5]}>
           You will access your real medical data using Discovery in t
           he safest manner possible.
         </Text>
-        <Text style={[h5, alignCenter, mb5]}>
+        <Text style={[h6, alignCenter, mb5]}>
           We will never see or share your data. Encrypted medical
           data may be stored only on your phone, visible to only you
           and nobody else.
         </Text>
         <TouchableOpacity>
-          <Text style={[h5, alignCenter, mb5, styles.button]}>
+          <Text style={[h6, alignCenter, mb5, styles.button]}>
             Read more about safety
           </Text>
         </TouchableOpacity>
