2b480276d64a51b0b640baf5d0f816dcb0c6e42d
Gilbert Hsu <gilbert@vermonster.com>
2021-05-17 15:45:16 -0400

    OBScreenCollections4

--- src/navigation/OnboardingNavigator.js
+++ src/navigation/OnboardingNavigator.js
@@ -10,6 +10,7 @@ import OBScreenOrganization from '../screens/Onboarding/DataOrganization/OBScree
 import OBScreenCollections1 from '../screens/Onboarding/DataOrganization/OBScreenCollections1';
 import OBScreenCollections2 from '../screens/Onboarding/DataOrganization/OBScreenCollections2';
 import OBScreenCollections3 from '../screens/Onboarding/DataOrganization/OBScreenCollections3';
+import OBScreenCollections4 from '../screens/Onboarding/DataOrganization/OBScreenCollections4';
 
 const OnboardingStack = createStackNavigator();
 
@@ -27,6 +28,7 @@ const OnboardingNavigator = () => (
     <OnboardingStack.Screen name="Collections1" component={OBScreenCollections1} />
     <OnboardingStack.Screen name="Collections2" component={OBScreenCollections2} />
     <OnboardingStack.Screen name="Collections3" component={OBScreenCollections3} />
+    <OnboardingStack.Screen name="Collections4" component={OBScreenCollections4} />
   </OnboardingStack.Navigator>
 );
 
