4598149887450469d7ddb2ef91cdb70280848dc5
Gilbert Hsu <gilbert@vermonster.com>
2021-05-18 16:48:25 -0400

    splash fade animation

--- src/screens/Onboarding/OBScreenSplash.js
+++ src/screens/Onboarding/OBScreenSplash.js
@@ -1,13 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import {
-  StyleSheet, Text, View, Image, SafeAreaView,
+  StyleSheet, Text, View, Image, SafeAreaView, Animated,
 } from 'react-native';
 
-import { shape } from 'prop-types';
+import { node, shape } from 'prop-types';
 import TextStyles from '../../constants/TextStyles';
 import discoveryFull from '../../../assets/images/discovery-full-logo.png';
 import Colors from '../../constants/Colors';
 import vermonsterLogo from '../../../assets/images/vermonster-logo.png';
+
+const FadeInView = ({ children }) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(
+      fadeAnim,
+      {
+        toValue: 1,
+        duration: 1000,
+        useNativeDriver: true,
+      },
+    ).start();
+  }, [fadeAnim]);
+
+  return (
+    <Animated.View style={[styles.animatedView, { opacity: fadeAnim }]}>
+      {children}
+    </Animated.View>
+  );
+};
+
+FadeInView.propTypes = {
+  children: node.isRequired,
+};
 
 const OBScreenSplash = ({ navigation }) => {
   const {
@@ -20,22 +45,24 @@ const OBScreenSplash = ({ navigation }) => {
 
   return (
     <SafeAreaView style={styles.root}>
-      <Image
-        style={styles.discoverylogo}
-        source={discoveryFull}
-        resizeMode="contain"
-      />
-      <View style={styles.companyLogosContainer}>
-        <Text style={[h5, mb4]}>Powered By</Text>
-        <View style={styles.harvard}>
-          <Text>Harvard Logo</Text>
-        </View>
+      <FadeInView>
         <Image
-          style={styles.vermonster}
-          source={vermonsterLogo}
+          style={styles.discoverylogo}
+          source={discoveryFull}
           resizeMode="contain"
         />
-      </View>
+        <View style={styles.companyLogosContainer}>
+          <Text style={[h5, mb4]}>Powered By</Text>
+          <View style={styles.harvard}>
+            <Text>Harvard Logo</Text>
+          </View>
+          <Image
+            style={styles.vermonster}
+            source={vermonsterLogo}
+            resizeMode="contain"
+          />
+        </View>
+      </FadeInView>
     </SafeAreaView>
   );
 };
@@ -71,5 +98,10 @@ const styles = StyleSheet.create({
   },
   vermonster: {
     height: 50,
+  },
+  animatedView: {
+    alignItems: 'center',
+    width: '100%',
+    flex: 1,
   },
 });
