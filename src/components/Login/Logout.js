f7d903133bf5a3eea67fac65eb88c29fe44b50bb
erwin foxtree <foxtree@users.noreply.github.com>
2021-05-18 11:43:52 -0400

    clear authentication from recoil

--- src/components/Login/Logout.js
+++ src/components/Login/Logout.js
@@ -1,16 +1,21 @@
 import { cloneElement } from 'react';
 import { element, func } from 'prop-types';
 import { connect } from 'react-redux';
+import { useResetRecoilState } from 'recoil';
 
 import { clearAuth } from '../../features/auth/authSlice';
 import { actionTypes } from '../../redux/action-types';
+import { authenticationState } from '../../recoil';
 
 const Logout = ({
   children, clearAuthAction, clearPatientDataAction,
 }) => {
+  const resetAuthentication = useResetRecoilState(authenticationState);
+
   const handleLogout = () => {
     clearAuthAction();
     clearPatientDataAction();
+    resetAuthentication();
   };
 
   return cloneElement(children, { onPress: handleLogout });
