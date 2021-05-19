7225ab3047ba9ea45703736ebb2fcb9877f6133d
Gilbert Hsu <gilbert@vermonster.com>
2021-05-19 17:40:36 -0400

    wip

--- src/screens/Onboarding/DataAccess/OBScreenSecurity.js
+++ src/screens/Onboarding/DataAccess/OBScreenSecurity.js
@@ -17,25 +17,23 @@ const OBScreenSecurity = () => {
     h5, h6, body1, alignCenter, mb5,
   } = TextStyles;
   return (
-    <OBTemplate screenNumber={SCREEN_NUMBER} sectionTitle={SECTION_TITLE} nextScreen={NEXT_SCREEN}>
-      <View style={styles.default}>
-        <Text style={[h5, mb5]}>Security</Text>
-        <Text style={[body1, alignCenter, mb5]}>
-          You will access your real medical data using Discovery in t
-          he safest manner possible.
+    <View style={styles.default}>
+      <Text style={[h5, mb5]}>Security</Text>
+      <Text style={[body1, alignCenter, mb5]}>
+        You will access your real medical data using Discovery in t
+        he safest manner possible.
+      </Text>
+      <Text style={[body1, alignCenter, mb5]}>
+        We will never see or share your data. Encrypted medical
+        data may be stored only on your phone, visible to only you
+        and nobody else.
+      </Text>
+      <TouchableOpacity>
+        <Text style={[h6, alignCenter, mb5, styles.button]}>
+          Read more about safety
         </Text>
-        <Text style={[body1, alignCenter, mb5]}>
-          We will never see or share your data. Encrypted medical
-          data may be stored only on your phone, visible to only you
-          and nobody else.
-        </Text>
-        <TouchableOpacity>
-          <Text style={[h6, alignCenter, mb5, styles.button]}>
-            Read more about safety
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </OBTemplate>
+      </TouchableOpacity>
+    </View>
   );
 };
 
--- src/screens/Onboarding/OBScreenWalkthrough.js
+++ src/screens/Onboarding/OBScreenWalkthrough.js
@@ -1,52 +1,64 @@
-import React, {useRef, useState} from 'react'
-import {SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import Swiper from 'react-native-swiper'
-import OBHeader from './OBHeader'
-import OBNavigation from './OBNavigation'
+import React, { useRef, useState } from 'react';
+import {
+  SafeAreaView, StyleSheet, Text, View,
+} from 'react-native';
+import Swiper from 'react-native-swiper';
+import OBScreenSecurity from './DataAccess/OBScreenSecurity';
+
+import OBHeader from './OBHeader';
+import OBNavigation from './OBNavigation';
+import OBScreenWelcome from './OBScreenWelcome'
+
+const TOTAL_SCREEN_COUNT = 18
 
 const OBScreenWalkthrough = () => {
-  const [currentScreenIndex, setCurrentScreenIndex] = useState(0)
-  const swiperRef = useRef(null)
+  const [currentScreenIndex, setCurrentScreenIndex] = useState(0);
+  const swiperRef = useRef(null);
+  // const currentIndex = useRef(0)
+  // const currentIndex = swiperRef?.current?.state?.index
+  // console.log('swiperRef.current', swiperRef.current)
   const handlePressNext = () => {
-    setCurrentScreenIndex(currentScreenIndex + 1)
-    swiperRef.current.scrollBy(1, true)
-  }
+    swiperRef.current.scrollBy(1, true);
+  };
   const handlePressBack = () => {
-    setCurrentScreenIndex(currentScreenIndex - 1)
-    swiperRef.current.scrollBy(-1, true)
-  }
+    swiperRef.current.scrollBy(-1, true);
+  };
+  // const isFirstOrLast = currentIndex.current === 0 || currentIndex.current === (TOTAL_SCREEN_COUNT - 1)
+  // console.log('currentIndex', currentIndex)
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <View style={styles.root}>
-        <OBHeader />
+        <OBHeader 
+          // isFirstOrLast={isFirstOrLast}
+          // screenIndex={currentIndex}
+          // progressMarker={currentIndex}
+          totalProgressMarkers={TOTAL_SCREEN_COUNT}
+        />
         <Swiper
           ref={swiperRef}
           loop={false}
           index={0}
-          showsPagination={false}
+          showsPagination={true}
+          paginationStyle={{backgroundColor: 'red', justifyContent: 'flex-end'}}
+          // showsButtons={true}
+          // buttonWrapperStyle={{backgroundColor: 'red', alignItems: 'flex-end'}}
+          // onIndexChanged={(index) => setCurrentScreenIndex(index)}
         >
-          <View>
-            <Text>Hello</Text>
-          </View>
-          <View>
-            <Text>Bye</Text>
-          </View>
-          <View>
-            <Text>Bye</Text>
-          </View>
+          <OBScreenWelcome />
+          <OBScreenSecurity />
         </Swiper>
-        <OBNavigation 
-          screenIndex={currentScreenIndex} 
+        <OBNavigation
+          // screenIndex={currentScreenIndex}
           handlePressNext={handlePressNext}
           handlePressBack={handlePressBack}
-          totalScreenCount={18}
+          totalScreenCount={TOTAL_SCREEN_COUNT}
         />
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default OBScreenWalkthrough
+export default OBScreenWalkthrough;
 
 const styles = StyleSheet.create({
   safeAreaView: {
@@ -55,6 +67,6 @@ const styles = StyleSheet.create({
   },
   root: {
     flex: 1,
-    margin: 20
-  }
-})
+    margin: 20,
+  },
+});
