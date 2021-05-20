d7fea94c379ed9a1fc378e9b79b8c47a9ad60833
erwin foxtree <foxtree@users.noreply.github.com>
2021-05-20 10:58:00 -0400

    remove already-broken hardwareBackPress functionality

--- src/components/Login/Logout.js
+++ src/components/Login/Logout.js
@@ -1,10 +1,6 @@
-import React, { cloneElement } from 'react';
+import { cloneElement } from 'react';
 import { element, func } from 'prop-types';
 import { connect } from 'react-redux';
-import {
-  BackHandler,
-} from 'react-native';
-import { useFocusEffect } from '@react-navigation/native';
 
 import { clearAuth } from '../../features/auth/authSlice';
 import { actionTypes } from '../../redux/action-types';
@@ -16,14 +12,6 @@ const Logout = ({
     clearAuthAction();
     clearPatientDataAction();
   };
-
-  useFocusEffect(
-    React.useCallback(() => {
-      BackHandler.addEventListener('hardwareBackPress', handleLogout);
-
-      return () => BackHandler.removeEventListener('hardwareBackPress', handleLogout);
-    }, []),
-  );
 
   return cloneElement(children, { onPress: handleLogout });
 };
