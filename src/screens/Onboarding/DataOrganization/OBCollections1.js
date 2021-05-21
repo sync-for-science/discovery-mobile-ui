d9f37fe831350bc83b7370b939112b775d441722
Gilbert Hsu <gilbert@vermonster.com>
2021-05-21 15:16:12 -0400

    refactor dataOrganization

--- src/screens/Onboarding/DataOrganization/OBCollections1.js
+++ src/screens/Onboarding/DataOrganization/OBCollections1.js
@@ -1,68 +1,31 @@
 import React from 'react';
 import {
-  StyleSheet, Text, View, Image,
+  StyleSheet, Text, View,
 } from 'react-native';
 
-import TextStyles from '../../../constants/TextStyles';
-import discoveryBasic from '../../../../assets/images/discovery-basic-logo.png';
 import Colors from '../../../constants/Colors';
+import OBSectionBodyTemplate from '../components/OBSectionBodyTemplate';
 
 // wireframe page 14
-const OBCollections1 = () => {
-  const {
-    h2, h4, alignCenter, mb5,
-  } = TextStyles;
-  return (
-    <View style={styles.root}>
-      <View style={styles.contentContainer}>
-        <View style={styles.titleContainer}>
-          <Image
-            style={[styles.logo, mb5]}
-            source={discoveryBasic}
-            resizeMode="contain"
-          />
-          <Text style={[h2, alignCenter, mb5]}>Data Organization</Text>
-        </View>
-        <Text style={[h4, alignCenter, mb5]}>
-          Collections
-        </Text>
-        <Text style={[alignCenter, mb5, styles.body]}>
-          Create a new Collection or access an existing Collection.
-        </Text>
-        <View style={styles.image}>
-          <Text>Image</Text>
-        </View>
-      </View>
+const OBCollections1 = () => (
+  <OBSectionBodyTemplate title="Data Organization" subTitle="Collections">
+    <Text style={styles.body}>
+      Create a new Collection or access an existing Collection.
+    </Text>
+    <View style={styles.image}>
+      <Text>Image</Text>
     </View>
-  );
-};
+  </OBSectionBodyTemplate>
+);
 
 export default OBCollections1;
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    justifyContent: 'space-between',
-  },
-  contentContainer: {
-    width: '100%',
-  },
-  logo: {
-    height: 60,
-    width: 60,
-    marginRight: 10,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-    justifyContent: 'center',
-  },
   body: {
     fontSize: 18,
     lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   image: {
     width: '100%',
--- src/screens/Onboarding/DataOrganization/OBCollections4.js
+++ src/screens/Onboarding/DataOrganization/OBCollections4.js
@@ -1,76 +1,39 @@
 import React from 'react';
 import {
-  StyleSheet, Text, View, Image,
+  StyleSheet, Text, View,
 } from 'react-native';
 
-import TextStyles from '../../../constants/TextStyles';
-import discoveryBasic from '../../../../assets/images/discovery-basic-logo.png';
 import Colors from '../../../constants/Colors';
+import OBSectionBodyTemplate from '../components/OBSectionBodyTemplate';
 
 // wireframe page 17
-const OBCollections4 = () => {
-  const {
-    h2, h4, alignCenter, mb5,
-  } = TextStyles;
-  return (
-    <View style={styles.root}>
-      <View style={styles.contentContainer}>
-        <View style={styles.titleContainer}>
-          <Image
-            style={[styles.logo, mb5]}
-            source={discoveryBasic}
-            resizeMode="contain"
-          />
-          <Text style={[h2, alignCenter, mb5]}>Data Organization</Text>
-        </View>
-        <Text style={[h4, alignCenter, mb5]}>
-          Collections
-        </Text>
-        <Text style={[alignCenter, mb5, styles.body]}>
-          You will be able to add notes to a Collection that can have
-          many purposes: wrapping meaning around the Records, logging
-          personal information, or journaling.
-        </Text>
-        <Text style={[alignCenter, mb5, styles.body]}>
-          Access the notes for the Collection.
-        </Text>
-        <Text style={[alignCenter, mb5, styles.body]}>
-          Add, remove or modify notes about the Collection.
-        </Text>
-        <View style={styles.image}>
-          <Text>Image</Text>
-        </View>
-      </View>
+const OBCollections4 = () => (
+  <OBSectionBodyTemplate title="Data Organization" subTitle="Collections">
+    <Text style={styles.body}>
+      You will be able to add notes to a Collection that can have
+      many purposes: wrapping meaning around the Records, logging
+      personal information, or journaling.
+    </Text>
+    <Text style={styles.body}>
+      Access the notes for the Collection.
+    </Text>
+    <Text style={styles.body}>
+      Add, remove or modify notes about the Collection.
+    </Text>
+    <View style={styles.image}>
+      <Text>Image</Text>
     </View>
-  );
-};
+  </OBSectionBodyTemplate>
+);
 
 export default OBCollections4;
 
 const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-    alignItems: 'center',
-    paddingHorizontal: 20,
-    justifyContent: 'space-between',
-  },
-  contentContainer: {
-    width: '100%',
-  },
-  logo: {
-    height: 60,
-    width: 60,
-    marginRight: 10,
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginBottom: 20,
-  },
   body: {
     fontSize: 18,
     lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   image: {
     width: '100%',
