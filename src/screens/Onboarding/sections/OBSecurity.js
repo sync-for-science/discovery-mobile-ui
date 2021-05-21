be8510e13923de958ba90a98ca518795134306c5
Gilbert Hsu <gilbert@vermonster.com>
2021-05-21 15:30:15 -0400

    refactor sections

--- src/screens/Onboarding/sections/OBSecurity.js
+++ src/screens/Onboarding/sections/OBSecurity.js
@@ -3,32 +3,26 @@ import {
   StyleSheet, Text, Button,
 } from 'react-native';
 
-import TextStyles from '../../../constants/TextStyles';
 import OBSectionCoverTemplate from '../components/OBSectionCoverTemplate';
 
 // wireframe page 5
-const OBSecurity = () => {
-  const {
-    alignCenter, mb5,
-  } = TextStyles;
-  return (
-    <OBSectionCoverTemplate title="Security">
-      <Text style={[styles.body, alignCenter, mb5]}>
-        Discovery pulls your medical records from your
-        providers in a secure encrypted  manner.
-      </Text>
-      <Text style={[styles.body, alignCenter, mb5]}>
-        Your records are kept encrypted on the phone and
-        are only visible while using the Discovery App.
-      </Text>
-      <Text style={[styles.body, alignCenter, mb5]}>
-        They are never copied or shared with any other
-        application or service.
-      </Text>
-      <Button title="Read more about safety" />
-    </OBSectionCoverTemplate>
-  );
-};
+const OBSecurity = () => (
+  <OBSectionCoverTemplate title="Security">
+    <Text style={styles.body}>
+      Discovery pulls your medical records from your
+      providers in a secure encrypted  manner.
+    </Text>
+    <Text style={styles.body}>
+      Your records are kept encrypted on the phone and
+      are only visible while using the Discovery App.
+    </Text>
+    <Text style={styles.body}>
+      They are never copied or shared with any other
+      application or service.
+    </Text>
+    <Button title="Read more about safety" />
+  </OBSectionCoverTemplate>
+);
 
 export default OBSecurity;
 
@@ -36,5 +30,7 @@ const styles = StyleSheet.create({
   body: {
     fontSize: 18,
     lineHeight: 24,
+    textAlign: 'center',
+    marginBottom: 20,
   },
 });
