b1568a6e08dc7f69f50bfc017e039026f77b5aba
Gilbert Hsu <gilbert@vermonster.com>
2021-05-21 09:53:12 -0400

    OBVideo

---
+++ src/screens/Onboarding/OBVideo.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {
+  StyleSheet, Text, View, Image,
+} from 'react-native';
+
+import TextStyles from '../../constants/TextStyles';
+import discoveryBasic from '../../../assets/images/discovery-basic-logo.png';
+import Colors from '../../constants/Colors';
+
+// wireframe page 254
+const OBVideo = () => {
+  const {
+    h2, alignCenter, mb5,
+  } = TextStyles;
+  return (
+    <View style={styles.root}>
+      <Text style={[h2, alignCenter, mb5]}>Video Tutorial</Text>
+      <Image
+        style={[styles.logo, mb5]}
+        source={discoveryBasic}
+        resizeMode="contain"
+      />
+      <Text style={[styles.body, alignCenter, mb5]}>
+        See how Discovery works on a fictitious patient!
+      </Text>
+      <View style={styles.image}>
+        <Text>Video</Text>
+      </View>
+    </View>
+  );
+};
+
+export default OBVideo;
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    paddingHorizontal: 20,
+  },
+  logo: {
+    height: 60,
+  },
+  body: {
+    fontSize: 18,
+    lineHeight: 24,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    backgroundColor: Colors.lightgrey2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
