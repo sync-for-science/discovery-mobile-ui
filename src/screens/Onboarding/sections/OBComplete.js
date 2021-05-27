2a0adce9a2cc5cb27b33ac405acf8abd55e3f1b6
Gilbert Hsu <gilbert@vermonster.com>
2021-05-27 17:50:49 -0400

    refactor OBComplete

--- src/screens/Onboarding/sections/OBComplete.js
+++ src/screens/Onboarding/sections/OBComplete.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import {
-  StyleSheet, Text, View,
+  StyleSheet, Text, TouchableOpacity, View,
 } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; // eslint-disable-line import/no-extraneous-dependencies
+import { func } from 'prop-types';
 
 import TextStyles from '../../../constants/TextStyles';
 import Colors from '../../../constants/Colors';
 import LogoBasic from '../../../icons/LogoBasic';
 
-// wireframe page 25
-const OBComplete = () => {
+// wireframe page 24
+const OBComplete = ({ handleOnboardingState }) => {
   const {
-    h2, alignCenter, mb5,
+    h2, alignCenter,
   } = TextStyles;
   return (
     <View style={styles.root}>
-      <Text style={[h2, alignCenter, mb5]}>THANK YOU [USER], YOU’RE ALL SET!</Text>
-      <View style={styles.logoContainer}>
-        <LogoBasic height={60} width={60} />
+      <View style={styles.thankYouContainer}>
+        <Text style={[h2, alignCenter, styles.thankYouText]}>Thank you Jonathan,</Text>
+        <Text style={[h2, alignCenter, styles.thankYouText]}>you&apos;re all set!</Text>
       </View>
-
+      <TouchableOpacity onPress={() => handleOnboardingState(true)}>
+        <View style={styles.logoContainer}>
+          <LogoBasic height={150} width={150} />
+        </View>
+        <View style={styles.tapToStartContainer}>
+          <MaterialCommunityIcons name="chevron-double-up" size={60} color={Colors.logoBlue} />
+          <Text style={[styles.body, styles.tapToStartText]}>Tap to Start</Text>
+        </View>
+      </TouchableOpacity>
     </View>
   );
+};
+
+OBComplete.propTypes = {
+  handleOnboardingState: func.isRequired,
 };
 
 export default OBComplete;
@@ -29,10 +43,10 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
     alignItems: 'center',
-    paddingHorizontal: 20,
+    justifyContent: 'center',
   },
   logoContainer: {
-    marginBottom: 20,
+    marginBottom: 8,
   },
   body: {
     fontSize: 18,
@@ -46,5 +60,21 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lightgrey2,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  thankYouContainer: {
+    width: '100%',
+    backgroundColor: Colors.logoBlue,
+    padding: 8,
+    marginBottom: 24,
+  },
+  thankYouText: {
+    color: 'white',
+  },
+  tapToStartContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tapToStartText: {
+    color: Colors.logoBlue,
   },
 });
