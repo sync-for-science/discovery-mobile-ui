03c54776d6d4862653951052e338839c063d4962
erwin foxtree <foxtree@users.noreply.github.com>
2021-06-01 16:22:39 -0400

    app.config.ts: export and lint

--- app.config.ts
+++ app.config.ts
@@ -1,40 +1,40 @@
-{
-  "expo": {
-    "name": "discovery-mobile-ui",
-    "slug": "discovery-mobile-ui",
-    "owner": "sync-for-science",
-    "version": "0.2.0",
-    "orientation": "portrait",
-    "icon": "./assets/icon.png",
-    "splash": {
-      "image": "./assets/splash.png",
-      "resizeMode": "contain",
-      "backgroundColor": "#ffffff"
+module.exports = {
+  expo: {
+    name: 'discovery-mobile-ui',
+    slug: 'discovery-mobile-ui',
+    owner: 'sync-for-science',
+    version: '0.2.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
     },
-    "updates": {
-      "fallbackToCacheTimeout": 0
+    updates: {
+      fallbackToCacheTimeout: 0,
     },
-    "assetBundlePatterns": [
-      "**/*"
+    assetBundlePatterns: [
+      '**/*',
     ],
-    "ios": {
-      "bundleIdentifier": "com.s4s.discoverymobile",
-      "buildNumber": "0.2.0",
-      "supportsTablet": true,
-      "config": {
-        "usesNonExemptEncryption": false
-      }
+    ios: {
+      bundleIdentifier: 'com.s4s.discoverymobile',
+      buildNumber: '0.2.0',
+      supportsTablet: true,
+      config: {
+        usesNonExemptEncryption: false,
+      },
     },
-    "android": {
-      "package": "com.s4s.discoverymobile",
-      "versionCode": 5,
-      "adaptiveIcon": {
-        "foregroundImage": "./assets/adaptive-icon.png",
-        "backgroundColor": "#FFFFFF"
-      }
+    android: {
+      package: 'com.s4s.discoverymobile',
+      versionCode: 5,
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
     },
-    "web": {
-      "favicon": "./assets/favicon.png"
-    }
-  }
-}
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+};
