7f9bc83e71f46efae5702ddfee5b9f31b4a3f4fc
Gilbert Hsu <gilbert@vermonster.com>
2021-06-01 15:25:37 -0400

    spacing > responsiveDimensinos

---
+++ src/constants/ResponsiveDimensions.js
@@ -0,0 +1,9 @@
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+export default {
+  rd1: hp('0.4%'), // approx 4px @ iphone 11
+  rd2: hp('0.8%'), // approx 8px
+  rd3: hp('1.2%'), // approx 12px 
+  rd4: hp('1.6%'), // approx 16px
+  rd5: hp('2.0%'), // approx 20px
+};
