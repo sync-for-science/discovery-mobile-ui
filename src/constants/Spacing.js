57c854332f409d2ab3cf7bc82e6f3bce19887b40
Gilbert Hsu <gilbert@vermonster.com>
2021-06-01 15:25:37 -0400

    spacing > responsiveDimensinos

--- src/constants/Spacing.js
+++
@@ -1,9 +0,0 @@
-import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
-
-export default {
-  s1: hp('0.4%'),
-  s2: hp('0.8%'),
-  s3: hp('1.2%'),
-  s4: hp('1.6%'),
-  s5: hp('2.0%'),
-};
