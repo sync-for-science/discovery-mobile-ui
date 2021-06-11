745ed05c1ac81b5843731bd7efbc47defaf5846e
erwin foxtree <foxtree@users.noreply.github.com>
2021-06-11 15:30:41 -0400

    tabbed interface

--- src/screens/CollectionsListScreen.js
+++ src/screens/CollectionsListScreen.js
@@ -1,36 +1,68 @@
 import React from 'react';
 import {
+  SafeAreaView,
+  StatusBar,
   StyleSheet,
 } from 'react-native';
 import { connect } from 'react-redux';
 import { shape } from 'prop-types';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
+import { collectionsSelector } from '../redux/selectors';
 import CollectionsIndex from '../components/Collections/index';
+import Colors from '../constants/Colors';
 
-const CollectionsListScreen = ({
-  navigation,
-  collections,
-}) => {
-  console.info('navigation: ', navigation);
-  return (
-    <CollectionsIndex
-      navigation={navigation}
-      collections={collections}
-    />
-  );
-};
+const CollectionsIndexCustom = connect((state) => ({
+  collections: collectionsSelector(state),
+}), null)(({ navigation, collections }) => (
+  <CollectionsIndex
+    navigation={navigation}
+    collections={collections}
+  />
+));
+
+const CollectionsIndexPrebuilt = connect((state) => ({
+  collections: collectionsSelector(state),
+}), null)(({ navigation, collections }) => (
+  <CollectionsIndex
+    navigation={navigation}
+    collections={collections}
+  />
+));
+
+const Tab = createMaterialTopTabNavigator();
+
+const CollectionsListScreen = () => (
+  <SafeAreaView style={styles.root}>
+    <StatusBar backgroundColor={Colors.primary} barStyle="dark-content" />
+    <Tab.Navigator
+      initialRouteName="Updates"
+      style={styles.tabs}
+    >
+      <Tab.Screen
+        name="Builds"
+        component={CollectionsIndexCustom}
+      />
+      <Tab.Screen
+        name="Updates"
+        component={CollectionsIndexPrebuilt}
+      />
+    </Tab.Navigator>
+  </SafeAreaView>
+);
 
 CollectionsListScreen.propTypes = {
-  navigation: shape({}).isRequired,
-  collections: shape({}).isRequired,
+  navigation: shape({}).isRequired, // eslint-disable-line react/no-unused-prop-types
 };
 
-const mapStateToProps = (state) => ({
-  collections: state.collections,
-});
-
-export default connect(mapStateToProps, null)(CollectionsListScreen);
+export default CollectionsListScreen;
 
 const styles = StyleSheet.create({
-
+  root: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  scrollContainer: {
+    flex: 1,
+  },
 });
