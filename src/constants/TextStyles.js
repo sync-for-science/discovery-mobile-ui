a567ef0abf9aac3af845464579c0c20bd0f6886c
Gilbert Hsu <gilbert@vermonster.com>
2021-06-14 15:40:02 -0400

    refactor text styles

--- src/constants/TextStyles.js
+++ src/constants/TextStyles.js
@@ -1,39 +1,53 @@
+import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
 export default {
   h1: {
-    fontSize: 36,
+    fontSize: hp('4.5%'),
     fontWeight: '700',
   },
   h2: {
-    fontSize: 32,
+    fontSize: hp('4.0%'),
     fontWeight: '700',
   },
   h3: {
-    fontSize: 28,
+    fontSize: hp('3.5%'),
     fontWeight: '700',
   },
   h4: {
-    fontSize: 24,
+    fontSize: hp('3.0%'),
     fontWeight: '600',
   },
   h5: {
-    fontSize: 20,
+    fontSize: hp('2.5%'),
     fontWeight: '600',
   },
   h6: {
-    fontSize: 16,
+    fontSize: hp('2.0%'),
     fontWeight: '600',
   },
   subtitle1: {
+    fontSize: hp('1.8%'),
     fontWeight: '600',
   },
   subtitle2: {
+    fontSize: hp('1.8%'),
     fontStyle: 'italic',
   },
   body1: {
-    // intentionally left empty
+    fontSize: hp('1.8%'),
+    lineHeight: hp('2.4%'),
   },
   body2: {
-    fontSize: 12,
+    fontSize: hp('1.5%'),
+    ineHeight: hp('1.8%'),
+  },
+  body3: { // onboarding body text
+    fontSize: hp('2.0%'),
+    lineHeight: hp('2.8%'),
+  },
+  body4: { // onboarding subtitle text
+    fontSize: hp('2.5%'),
+    lineHeight: hp('3.5%'),
   },
   alignLeft: {
     textAlign: 'left',
