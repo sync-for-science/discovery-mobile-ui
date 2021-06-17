e827e18455e2d5cc463dd14b2d5fed1efd926bdd
Gilbert Hsu <gilbert@vermonster.com>
2021-06-17 14:00:13 -0400

    wip new tab

---
+++ src/screens/UpdatesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const UpdatesScreen = () => {
+  return (
+    <View>
+      <Text>UpdatesScreen</Text>
+    </View>
+  )
+}
+
+export default UpdatesScreen
+
+const styles = StyleSheet.create({})
