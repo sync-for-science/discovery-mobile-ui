959a33a4f04869d74afbb446a142c6d8a2e9b10e
Gilbert Hsu <gilbert@vermonster.com>
2021-06-17 10:40:46 -0400

    wip show description text

--- src/components/CollectionRow/PreBuiltDescriptionText.js
+++ src/components/CollectionRow/PreBuiltDescriptionText.js
@@ -1,33 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
 import { connect } from 'react-redux';
-import { number, string } from 'prop-types';
+import { number, string, arrayOf } from 'prop-types';
 
 import { preBuiltDatesSelector, encountersCountSelector } from '../../redux/selectors';
 import { PREBUILT_COLLECTIONS_LABELS } from '../../redux/reducers';
+import { formatDate } from '../../resources/fhirReader'
 
-const PreBuiltDescriptionText = ({ preBuiltDates, encountersCount }) => {
+const PreBuiltDescriptionText = ({ preBuiltDates, encountersCount, collectionId}) => {
   console.log('encountersCount', encountersCount);
   console.log('preBuiltDates', preBuiltDates);
-  // const thereAreText = datesArray.count < 5 ? 'However, there' : 'There';
-  // const collectionLabel = ownProps.collectionId === PREBUILT_COLLECTIONS_LABELS.lastLabResults ? 'Lab Results' : 'Vital Signs';
-  // const printDates = datesArray.reduce((acc, date) => acc.concat(`\n   ${formatDate(date)}`), '');
-  // if (ownProps.collectionId === PREBUILT_COLLECTIONS_LABELS.lastEncounters) {
-  //   return (<Text style={{color: 'red'}}>This Collection is supposed to identify the last 3 Encounters. There are ${encountersCount} such Encounters in your data.</Text>);
-  // }
-
-  // return `This Collection is supposed to identify the ${collectionLabel} for you the last 5 dates they were received. ${thereAreText} are ${datesArray.length} dates found in your Records: ${printDates}`;
+  const thereAreText = preBuiltDates.count < 5 ? 'However, there' : 'There';
+  const collectionLabel = collectionId === PREBUILT_COLLECTIONS_LABELS.lastLabResults ? 'Lab Results' : 'Vital Signs';
+  const printDates = preBuiltDates.reduce((acc, date) => acc.concat(`\n   ${formatDate(date)}`), '');
+  if (collectionId === PREBUILT_COLLECTIONS_LABELS.lastEncounters) {
+    return (
+      <Text style={{color: 'red'}}>
+        This Collection is supposed to identify the last 3 Encounters. 
+        There are {encountersCount} such Encounters in your data.
+      </Text>
+    );
+  }
 
   return (
-    <View>
-      <Text>PreBuiltDescriptionText</Text>
-    </View>
-  );
+    <Text>
+      This Collection is supposed to identify the {collectionLabel} 
+      for you the last 5 dates they were received. {thereAreText} are 
+      {preBuiltDates.length} dates found in your Records: {printDates}
+    </Text>
+  )
 };
 
 PreBuiltDescriptionText.propTypes = {
   preBuiltDates: arrayOf(string).isRequired,
   encountersCount: number.isRequired,
+  collectionId: string.isRequired
 };
 
 const mapStateToProps = (state, ownProps) => ({
