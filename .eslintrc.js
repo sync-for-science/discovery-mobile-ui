be388af445bbbf6eeeaffa1cf7324b9c2c900f11
erwin foxtree <foxtree@users.noreply.github.com>
2021-06-24 09:40:28 -0400

    .eslintrc:
    * settings
    
    * import/extensions: ts into js
    
    * boost max-len from 100 > 140, for .ts
    
    * tsconfig.json, types/s4s/index.d.ts
    
    * typescript eslint integration

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,7 +21,67 @@ module.exports = {
     'react-native',
   ],
   rules: {
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
     'no-use-before-define': ['error', { functions: true, classes: true, variables: false }], // disable the rule for variables, but enable it for functions and classes
     'react/jsx-filename-extension': ['off'],
   },
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        // extensions: ['.js', '.jsx', '.ts', '.tsx', '.d.ts'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'airbnb',
+        // 'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+      plugins: [
+        '@typescript-eslint',
+        'react',
+        'react-native',
+      ],
+      rules: {
+        'no-use-before-define': 'off',
+        'max-len': [
+          'error',
+          140,
+          2,
+          {
+            ignoreUrls: true,
+            ignoreComments: false,
+            ignoreRegExpLiterals: true,
+            ignoreStrings: true,
+            ignoreTemplateLiterals: true,
+          },
+        ],
+        '@typescript-eslint/no-use-before-define': ['error'],
+      },
+    },
+  ],
 };
