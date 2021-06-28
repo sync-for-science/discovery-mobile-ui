63ba29fab967062489a58d89e3dd8bafaa1f67e6
erwin foxtree <foxtree@users.noreply.github.com>
2021-06-28 16:22:21 -0400

    fhirReader.ts: return types

--- src/resources/fhirReader.ts
+++ src/resources/fhirReader.ts
@@ -8,7 +8,7 @@ const UI_DATE_FORMAT_SHORT_YEAR = "MMM d, ''yy";
 const UI_DATE_FORMAT_LONG = 'MMM d, y h:mm:ssaaa';
 const UI_DATE_FORMAT_SHORT = 'MM/dd/y';
 
-export const getPatientName = (patientResource: fhir4.Patient) => {
+export const getPatientName = (patientResource: fhir4.Patient) : string => {
   const patientName = patientResource?.name?.[0];
   if (!patientName) {
     return '';
@@ -17,7 +17,7 @@ export const getPatientName = (patientResource: fhir4.Patient) => {
   return [given?.[0], family].join(' ');
 };
 
-export const formatPractitionerName = (practitionerResource: fhir4.Practitioner) => {
+export const formatPractitionerName = (practitionerResource: fhir4.Practitioner) : string => {
   const practitionerName = practitionerResource?.name?.[0];
   if (practitionerName) {
     const { prefix, given, family } = practitionerName;
@@ -26,22 +26,22 @@ export const formatPractitionerName = (practitionerResource: fhir4.Practitioner)
   return '';
 };
 
-export const getPatientGender = (patientResource: fhir4.Patient) => patientResource?.gender;
+export const getPatientGender = (patientResource: fhir4.Patient) : string => patientResource?.gender || '';
 
 // returns human-readable patient birth date
-export const formatPatientBirthDate = (patientResource: fhir4.Patient) => {
+export const formatPatientBirthDate = (patientResource: fhir4.Patient) : string => {
   if (!patientResource?.birthDate) {
-    return null;
+    return '';
   }
   const birthDate = parse(patientResource.birthDate, 'yyyy-MM-dd', new Date());
   return format(birthDate, UI_DATE_FORMAT);
 };
 
-export const getPatientAddresses = (patientResource: fhir4.Patient) => patientResource?.address;
+export const getPatientAddresses = (patientResource: fhir4.Patient) : fhir4.Address[] | undefined => patientResource?.address;
 
-export const formatAddress = (address: fhir4.Address[]) => {
+export const formatAddress = (address: fhir4.Address[]) : string => {
   if (!address) {
-    return null;
+    return '';
   }
   // handle the first one for now
   const firstAddress = address[0];
@@ -56,9 +56,9 @@ export const formatAddress = (address: fhir4.Address[]) => {
 
 // TODO: make it handle only years or months which is valid
 // TODO: have it return months for babies
-export const getPatientAge = (patient: fhir4.Patient) => {
+export const getPatientAge = (patient: fhir4.Patient) : string => {
   if (!patient?.birthDate) {
-    return null;
+    return '';
   }
   const { birthDate } = patient;
   const birthDuration = intervalToDuration(
@@ -69,7 +69,7 @@ export const getPatientAge = (patient: fhir4.Patient) => {
   );
 
   if (birthDuration.years === undefined) {
-    return null;
+    return '';
   }
 
   return formatDuration(birthDuration, birthDuration.years > 5 ? { format: ['years'] } : { format: ['years', 'months'] });
@@ -84,60 +84,60 @@ export const getPatientAge = (patient: fhir4.Patient) => {
 // };
 
 // TODO: Ensure these always receive a Date or empty value, and perform Date(...) casting in caller?
-export const formatDate = (date: string | Date | undefined) => (date ? format(new Date(date), UI_DATE_FORMAT) : '');
+export const formatDate = (date: string | Date | undefined) : string => (date ? format(new Date(date), UI_DATE_FORMAT) : '');
 
-export const formatDateShortYear = (date: string) => (date ? format(new Date(date), UI_DATE_FORMAT_SHORT_YEAR) : '');
+export const formatDateShortYear = (date: string) : string => (date ? format(new Date(date), UI_DATE_FORMAT_SHORT_YEAR) : '');
 
-export const formatDateTime = (date: string) => (date ? format(new Date(date), UI_DATE_FORMAT_LONG) : '');
+export const formatDateTime = (date: string) : string => (date ? format(new Date(date), UI_DATE_FORMAT_LONG) : '');
 
-export const formatDateShort = (date: string) => (date ? format(new Date(date), UI_DATE_FORMAT_SHORT) : '');
+export const formatDateShort = (date: string) : string => (date ? format(new Date(date), UI_DATE_FORMAT_SHORT) : '');
 
-export const getResourceDate = (resource: s4s.Resource) => (
+export const getResourceDate = (resource: s4s.Resource) : string => (
   resource.timelineDate ? formatDate(resource.timelineDate) : 'No Date Found'
 );
 
-const titleCase = (text: string | undefined) => (text ? text[0].toUpperCase() + text.substring(1).toLowerCase() : null);
+const titleCase = (text: string | undefined) : string => (text ? text[0].toUpperCase() + text.substring(1).toLowerCase() : '');
 
 // MedicationRequest, Procedure
-export const getReason = (resource: fhir4.MedicationRequest | fhir4.Procedure) => resource.reasonCode?.[0]?.coding?.[0]?.display;
+export const getReason = (resource: fhir4.MedicationRequest | fhir4.Procedure) : string => resource.reasonCode?.[0]?.coding?.[0]?.display || '';
 
-export const getOnsetDateTime = (resource: fhir4.Condition) => formatDate(resource.onsetDateTime);
+export const getOnsetDateTime = (resource: fhir4.Condition) : string => formatDate(resource.onsetDateTime);
 
-export const getAbatementDateTime = (resource: fhir4.Condition) => formatDate(resource.abatementDateTime);
+export const getAbatementDateTime = (resource: fhir4.Condition) : string => formatDate(resource.abatementDateTime);
 
 // only NutritionOrder has orderer?
-export const getOrderedBy = (resource: fhir4.Resource) => titleCase(resource.orderer?.display);
+export const getOrderedBy = (resource: fhir4.Resource) : string => titleCase(resource.orderer?.display);
 
 // assertedDate is an extension?
 //     http://www.hl7.org/FHIR/extension-condition-asserteddate.html
-export const getAssertedDate = (resource: fhir4.Condition) => formatDate(resource.assertedDate);
+export const getAssertedDate = (resource: fhir4.Condition) : string => formatDate(resource.assertedDate);
 
-export const getStatus = (resource: fhir4.Observation) => titleCase(resource.status);
+export const getStatus = (resource: fhir4.Observation) : string => titleCase(resource.status);
 
-export const getClinicalStatus = (resource: fhir4.Condition) => (
+export const getClinicalStatus = (resource: fhir4.Condition) : string => (
   titleCase(resource.clinicalStatus?.coding?.[0]?.code)
 );
 
-export const getVerficationStatus = (resource: fhir4.Condition) => (
+export const getVerficationStatus = (resource: fhir4.Condition) : string => (
   titleCase(resource.verificationStatus?.coding?.[0]?.code)
 );
 
-export const getEnding = (resource: fhir4.Encounter) => resource.period?.end && formatDateTime(resource.period.end);
+export const getEnding = (resource: fhir4.Encounter) : string => (resource.period?.end && formatDateTime(resource.period.end)) || '';
 
-export const getClass = (resource: fhir4.Encounter) => resource.class?.code;
+export const getClass = (resource: fhir4.Encounter) : string => resource.class?.code || '';
 
-export const getPrimarySource = (resource: fhir4.Immunization) => {
+export const getPrimarySource = (resource: fhir4.Immunization) : string => {
   const displayPrimarySource = resource.primarySource ? 'yes' : 'no';
   return titleCase(displayPrimarySource);
 };
 
-export const getValueRatio = (resource: fhir4.Observation) => (
-  resource.valueRatio ? `${resource.valueRatio?.numerator?.value} / ${resource.valueRatio?.denominator?.value}` : null
+export const getValueRatio = (resource: fhir4.Observation) : string => (
+  resource.valueRatio ? `${resource.valueRatio?.numerator?.value} / ${resource.valueRatio?.denominator?.value}` : ''
 );
 
-export const getRefRangeLabel = (resource: fhir4.Observation) => resource.referenceRange?.[0]?.meaning?.coding?.[0]?.display || 'REFERENCE RANGE';
+export const getRefRangeLabel = (resource: fhir4.Observation) : string => resource.referenceRange?.[0]?.meaning?.coding?.[0]?.display || 'REFERENCE RANGE';
 
-export const getRefRange = (resource: fhir4.Observation) => {
+export const getRefRange = (resource: fhir4.Observation) : string => {
   if (resource.referenceRange) {
     const lowValue = resource.referenceRange?.[0]?.low?.value;
     const lowUnits = resource.referenceRange?.[0]?.low?.unit;
@@ -148,12 +148,12 @@ export const getRefRange = (resource: fhir4.Observation) => {
       ? `${lowValue + (lowUnits && lowUnits !== highUnits ? ` ${lowUnits}` : '')} - ${highValue}${highUnits ? ` ${highUnits}` : ''}`
       : resource.referenceRange?.text;
   }
-  return null;
+  return '';
 };
 
-export const getValueQuantity = (resource: fhir4.Observation) => (resource.valueQuantity ? `${Number(resource.valueQuantity.value).toFixed(2)} ${resource.valueQuantity.unit}` : null);
+export const getValueQuantity = (resource: fhir4.Observation) : string => (resource.valueQuantity ? `${Number(resource.valueQuantity.value).toFixed(2)} ${resource.valueQuantity.unit}` : '');
 
-export const getBloodPressureData = (resource: fhir4.Observation) => {
+export const getBloodPressureData = (resource: fhir4.Observation) : Array<s4s.BloodPressureData> => {
   if (resource.component) {
     const bloodPressureData: Array<s4s.BloodPressureData> = [];
     resource.component.forEach((measurement: fhir4.ObservationComponent) => {
@@ -166,5 +166,5 @@ export const getBloodPressureData = (resource: fhir4.Observation) => {
     });
     return bloodPressureData;
   }
-  return null;
+  return [];
 };
