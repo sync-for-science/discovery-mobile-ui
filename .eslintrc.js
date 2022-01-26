2d391cbb7abdc352c78a971a2ddcc783e61b85e6
erwin foxtree <foxtree@users.noreply.github.com>
2022-01-26 11:38:59 -0500

    changes from: node_modules/.bin/eslint --init

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,16 +9,18 @@ module.exports = {
     'plugin:react/recommended',
     'airbnb',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 12,
+    ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [
     'react',
     'react-native',
+    '@typescript-eslint',
   ],
   rules: {
     'no-use-before-define': ['error', { functions: true, classes: true, variables: false }], // disable the rule for variables, but enable it for functions and classes
