0e346bae2b94c4bfb7b406b253f52e09ffc6c1ae
erwin foxtree <foxtree@users.noreply.github.com>
2022-02-11 16:34:16 -0500

    Login/index.tsx: fix Alert button type

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -37,7 +37,7 @@ const Login = () => {
       }
     } catch (error) {
       console.error('AppAuth Error:', error); // eslint-disable-line no-console
-      Alert.alert('Login Error', 'Must login to use Discovery', ['ok']);
+      Alert.alert('Login Error', 'You must log in to use Discovery', [{ text: 'OK' }]);
       // enable patient-picker and login buttons to render:
       setLoading(false);
     }
