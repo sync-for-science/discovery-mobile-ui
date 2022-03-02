e48c2fb8f38c11c304596239eb31f4076119894a
erwin foxtree <foxtree@users.noreply.github.com>
2022-03-02 17:14:00 -0500

    Support Expo Application Services (EAS) builds (#218)
    See: https://expo.dev/eas
    --------------------------------------------------------------------------------
    * metro.config.js must extend default config
    
    * package.json engines field: yarn ^1.22.10
    
    * eas build --platform ios
    
    * eas.json: build.production.node version 16.x
    
    * BUNDLE_IDENTIFIER from ENV
    
    * eas.json: build.preview.ios.enterpriseProvisioning = "universal"
    
    * increment version/ios.buildNumber to 0.2.3
    
    * log existence of ENVs, separately

--- app.config.ts
+++ app.config.ts
@@ -3,21 +3,28 @@ require('dotenv/config');
 const {
   EXPO_OWNER,
   EXPO_SLUG,
+  BUNDLE_IDENTIFIER,
   CLIENT_ID,
 } = process.env;
 
-if (!(EXPO_OWNER && EXPO_SLUG && CLIENT_ID)) {
-  throw new Error('EXPO_OWNER, EXPO_SLUG, and CLIENT_ID must be defined.');
+if (!(EXPO_OWNER && EXPO_SLUG && BUNDLE_IDENTIFIER && CLIENT_ID)) {
+  console.log('         EXPO_OWNER exists: ', !!EXPO_OWNER);
+  console.log('          EXPO_SLUG exists: ', !!EXPO_SLUG);
+  console.log('  BUNDLE_IDENTIFIER exists: ', !!BUNDLE_IDENTIFIER);
+  console.log('          CLIENT_ID exists: ', !!CLIENT_ID);
+  throw new Error('EXPO_OWNER, EXPO_SLUG, BUNDLE_IDENTIFIER, and CLIENT_ID must be defined.');
 }
 
 // https://docs.expo.dev/versions/latest/config/app/
+
+const VERSION = '0.2.3';
 
 module.exports = {
   expo: {
     name: 'Sync for Science Discovery',
     slug: EXPO_SLUG,
     owner: EXPO_OWNER,
-    version: '0.2.1',
+    version: VERSION,
     icon: './assets/icon.png',
     scheme: 'https',
     extra: {
@@ -39,15 +46,15 @@ module.exports = {
       '**/*',
     ],
     ios: {
-      bundleIdentifier: 'com.s4s.discoverymobile',
-      buildNumber: '0.2.0',
+      bundleIdentifier: BUNDLE_IDENTIFIER,
+      buildNumber: VERSION,
       supportsTablet: true,
       config: {
         usesNonExemptEncryption: false,
       },
     },
     android: {
-      package: 'com.s4s.discoverymobile',
+      package: BUNDLE_IDENTIFIER,
       versionCode: 5,
       adaptiveIcon: {
         foregroundImage: './assets/adaptive-icon.png',
--- metro.config.js
+++ metro.config.js
@@ -1,14 +1,17 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig(__dirname);
+  const config = await getDefaultConfig(__dirname);
+  const { transformer, resolver } = config;
+  const { sourceExts, assetExts } = resolver;
+
   return {
     transformer: {
+      ...transformer,
       babelTransformerPath: require.resolve('react-native-svg-transformer'),
     },
     resolver: {
+      ...resolver,
       assetExts: assetExts.filter((ext) => ext !== 'svg'),
       sourceExts: [...sourceExts, 'svg'],
     },
